468f46ff9d020eec9ab93363154611dd07bf90de
jgus52 <srraight@gmail.com>
2022-12-26 14:18:51 +0900

    Feat: 콤마로 구분된 숫자 split

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,7 @@
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Calculator {
     private String text;
@@ -9,8 +12,12 @@ public class Calculator {
     public int run() throws RuntimeException {
         if(this.isEmptyOrNull()) return 0;
 
+        return toInteger(this.text);
+    }
+
+    private int toInteger (String possibleNumber) throws RuntimeException {
         try {
-            int number = Integer.parseInt(text);
+            int number = Integer.parseInt(possibleNumber);
 
             if(number<0) throw new RuntimeException();
 
@@ -22,5 +29,14 @@ public class Calculator {
 
     public boolean isEmptyOrNull() {
         return text==null || text.equals("");
+    }
+
+    public List<Integer> splitText() {
+        String[] numbers = this.text.split(",");
+        List<Integer> arr = new ArrayList<>();
+
+        Arrays.stream(numbers).forEach(e-> arr.add(toInteger(e)));
+
+        return arr;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.*;
 public class CalculatorTest {
     @Test
@@ -34,5 +37,12 @@ public class CalculatorTest {
         Calculator calculator = new Calculator(text);
 
         assertThatExceptionOfType(RuntimeException.class).isThrownBy(calculator::run);
+    }
+
+    @Test
+    void checkSplitText(){
+        Calculator calculator = new Calculator("1,2");
+
+        assertThat(calculator.splitText()).isEqualTo(Arrays.asList(1, 2));
     }
 }
