be2b6e8f4ccc638f539b16f358d9a51e7acde71e
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-26 14:37:58 +0900

    test: customDelimiter 테스트 작성

--- src/test/java/StringSumTest.java
+++ src/test/java/StringSumTest.java
@@ -1,4 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringSumTest {
 
     private StringSum stringSum;
@@ -11,6 +15,12 @@ public class StringSumTest {
         stringSum = new StringSum("1:2,3");
 
         testList = new String[] {"1", "2", "3"};
+    }
+
+    @Test
+    public void customDelimiterTest() {
+        String customDelimiter = customStringSum.customDelimiter();
+        assertThat(customDelimiter).isEqualTo(";");
     }
 
 }
