586ec8288c07081318c6555cf474d8eb985536d2
kjaehyeon <jacob9404@gmail.com>
2022-12-26 14:42:21 +0900

    test: Calculator 요구사항 2 테스트 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,16 +1,39 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        this.calculator = new Calculator();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "     "})
     void givenEmptyOrNullString_thenReturnZero(String input){
-        Calculator calculator = new Calculator();
         int result = calculator.calculate(input);
 
         assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2"})
+    void givenOneDigitNum_thenReturnItSelf(String input){
+        int result = calculator.calculate(input);
+
+        assertThat(result).isEqualTo(Integer.parseInt(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "!"})
+    void givenOneChar_thenReturnException(String input) {
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(1);
     }
 }
