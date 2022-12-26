ffc2aee1ef7f1794ef4d4e645c1a9497e2b3508b
kjaehyeon <jacob9404@gmail.com>
2022-12-26 16:03:28 +0900

    test: Calculator 더하기 테스트

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 public class CalculatorTest {
     private Calculator calculator;
@@ -34,6 +38,23 @@ public class CalculatorTest {
     @ValueSource(strings = {"a", "!"})
     void givenOneChar_thenReturnException(String input) {
         int result = calculator.calculate(input);
+
         assertThat(result).isEqualTo(1);
     }
+
+    @ParameterizedTest
+    @MethodSource("generator")
+    void givenExpressionString_thenReturnSum(String input, Integer expected){
+        int result = calculator.calculate(input);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> generator(){
+        return Stream.of(
+                Arguments.of("1,2,3", 6),
+                Arguments.of("2;3,4", 9)
+        );
+    }
+
 }
