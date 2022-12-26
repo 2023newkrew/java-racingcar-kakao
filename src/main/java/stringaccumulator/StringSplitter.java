775d27826f47925e8ff0301e5d903d309464a287
정핸모 <gusah009@naver.com>
2022-12-26 16:51:18 +0900

    feat: 커스텀 구분자를 추출하는 기능 추가

---
+++ src/main/java/stringaccumulator/StringSplitter.java
@@ -0,0 +1,49 @@
+package stringaccumulator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSplitter {
+
+    private static final List<String> DEFAULT_SEPARATORS = List.of(",", ":");
+    private static final String PREFIX = "//";
+    private static final String SUFFIX = "\n";
+    private static final int PREFIX_LENGTH = PREFIX.length();
+    private static final int SUFFIX_LENGTH = SUFFIX.length();
+
+    List<String> separators;
+
+    public static StringSplitter from(String separatorFormat) {
+        return new StringSplitter(separatorFormat);
+    }
+
+    private StringSplitter(String separatorFormat) {
+        checkFormatValidation(separatorFormat);
+        separators = new ArrayList<>(DEFAULT_SEPARATORS);
+        addCustomSeparator(separatorFormat);
+    }
+
+    private static void checkFormatValidation(String separatorFormat) {
+        if (isInvalidFormat(separatorFormat)) {
+            throw new RuntimeException("Invalid separator format.");
+        }
+    }
+
+    private static boolean isInvalidFormat(String separatorFormat) {
+        return !separatorFormat.startsWith(PREFIX) || !separatorFormat.endsWith(SUFFIX);
+    }
+
+    private void addCustomSeparator(String separatorFormat) {
+        int lengthWithoutSuffix = separatorFormat.length() - SUFFIX_LENGTH;
+        String customSeparator = separatorFormat.substring(PREFIX_LENGTH, lengthWithoutSuffix);
+        separators.add(customSeparator);
+    }
+
+    public boolean contains(String separator) {
+        return separators.contains(separator);
+    }
+
+    public List<String> split(String content) {
+        return null;
+    }
+}
---
+++ src/test/java/stringaccumulator/StringSplitterTest.java
@@ -0,0 +1,51 @@
+package stringaccumulator;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringSplitterTest {
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class from {
+        @ParameterizedTest
+        @MethodSource
+        void should_returnStringSplitter_when_validString(String input, String customSeparator) {
+            StringSplitter splitter = StringSplitter.from(input);
+            assertThat(splitter.contains(customSeparator)).isTrue();
+        }
+
+        Stream<Arguments> should_returnStringSplitter_when_validString() {
+            return Stream.of(
+                    Arguments.of("//.\n", "."),
+                    Arguments.of("//. \n", ". "),
+                    Arguments.of("///.\n", "/."),
+                    Arguments.of("//.\n\n", ".\n"),
+                    Arguments.of("//123\n", "123")
+            );
+        }
+
+        @Test
+        void should_throwException_when_invalidPrefix() {
+            assertThatThrownBy(() -> {
+                StringSplitter.from("/.\n");
+            }).isInstanceOf(RuntimeException.class);
+        }
+
+        @Test
+        void should_throwException_when_invalidSuffix() {
+            assertThatThrownBy(() -> {
+                StringSplitter.from("//.\\");
+            }).isInstanceOf(RuntimeException.class);
+        }
+    }
+}
