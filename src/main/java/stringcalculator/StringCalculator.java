3376609c2298dfd0f7eefd66c2afbb9eac682071
ryo <ryo@kan.local>
2022-12-26 17:40:19 +0900

    feat: refactoring split function

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,22 +1,28 @@
 package stringcalculator;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String REGEX_PATTERN = "//(.)\n(.*)";
 
     public String[] split(String s) {
         if (s == null || s.isEmpty()) {
             return new String[]{};
         }
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        return splitByRegex(s).orElseGet(() -> s.split(DEFAULT_DELIMITER));
+    }
+
+    public Optional<String[]> splitByRegex(String s) {
+        Matcher m = Pattern.compile(REGEX_PATTERN).matcher(s);
         if (m.find()) {
             String delimiter = m.group(1);
-            return m.group(2).split(Pattern.quote(delimiter));
+            return Optional.of(m.group(2).split(Pattern.quote(delimiter)));
         }
-        return s.split(DEFAULT_DELIMITER);
+        return Optional.empty();
     }
 
     public int sumOf(String[] input) {
