af44cb2cde5d4d54d67efbd50cac85546ae43c0c
gkwodn1044 <gkwodn1044@gmail.com>
2022-12-26 16:44:00 +0900

    feat : RacingTest 자동차 이름 문자열 split 테스트 및 프로덕션 코드 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,10 +1,24 @@
 package racingcar;
 
 public class Race {
+    String nameStr;
+
+    public Race() {
+        this.nameStr = "pobi,crong,honux";
+    }
+
+    public Race(String nameStr) {
+        this.nameStr = nameStr;
+    }
+
     public boolean verifyName(String input) {
         if(input.length() < 6){
             return true;
         }
         return false;
+    }
+
+    public String[] nameSplit() {
+        return nameStr.split(",");
     }
 }
--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -1,8 +1,10 @@
 package racingcar;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingTest {
@@ -17,5 +19,11 @@ public class RacingTest {
             result = "true";
         }
         assertEquals(result, expected);
+    }
+
+    @Test
+    void nameSplitTest() {
+        Race race = new Race("pobi,crong,honux");
+        assertArrayEquals(race.nameSplit(), new String[] {"pobi", "crong", "honux"});
     }
 }
