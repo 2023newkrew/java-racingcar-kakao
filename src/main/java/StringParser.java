ce3b72a387950c50c0d5640363c5eebb3974180a
kakao <kakao@kakaoui-MacBookPro.local>
2022-12-26 15:21:14 +0900

    feat: add StringParser

---
+++ src/main/java/StringParser.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringParser {
+    private static final Character[] BASIC_SPLITTER = new Character[]{',', ':'};
+
+    public Character parseSplitter(String s) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        if (m.find()) {
+            return m.group(1).charAt(0);
+        }
+        return null;
+    }
+
+    public String[] splitTargetString(String s, Character splitter) {
+        ArrayList<Character> splitters = new ArrayList<Character>(List.of(BASIC_SPLITTER));
+
+        if (splitter != null) {
+            splitters.add(splitter);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+        for (Character ch : splitters) {
+            sb.append(ch);
+        }
+        sb.append("]");
+
+        return s.split(sb.toString(), -1);
+    }
+    public String parseTargetString(String s) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        if (m.find()) {
+            return m.group(2);
+        }
+        return s;
+    }
+
+}
