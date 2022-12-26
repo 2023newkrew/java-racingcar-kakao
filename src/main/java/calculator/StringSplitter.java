1c581645706662b92751d4780517505251f2d6eb
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-26 17:51:18 +0900

    feat: StringSplitter 기능 추가
    - 기본 구분자 ";" 추가
    - null, 공백 처리 추가
    - convertEmptyToZero(): 공백 문자열 0으로 변환

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -13,21 +13,31 @@ public class StringSplitter {
         if (hasCustomDelimiter()) {
             return input.substring(2, input.indexOf("\n"));
         }
-        return ",";
+        return ",|;";
     }
 
     public String[] split() {
         String str = input;
+        if (str==null || str.isEmpty()) return new String[]{"0"};
         if (hasCustomDelimiter()) {
             str = input.substring(input.indexOf("\n") + 1);
         }
-        return str.split(delimiter);
+        return convertEmptyToZero(str.split(delimiter));
     }
 
+
     public boolean hasCustomDelimiter() {
-        if (input.startsWith("//") && input.indexOf("\n") > 1) {
+        if (input!=null && input.startsWith("//") && input.indexOf("\n") > 1) {
             return true;
         }
         return false;
+    }
+
+    public String[] convertEmptyToZero(String[] arr) {
+        String[] result = new String[arr.length];
+        for (int i = 0; i<arr.length; i++) {
+            result[i] = arr[i].isEmpty() ? "0" : arr[i];
+        }
+        return  result;
     }
 }
