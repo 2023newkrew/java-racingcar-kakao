e03123c82c68a4d6aecddd0f3e712e2e2ba2933f
GrasshopperBears <starday456@naver.com>
2022-12-26 15:02:23 +0900

    feat: Main class 구현

---
+++ src/main/java/stringAdder/Main.java
@@ -0,0 +1,14 @@
+package stringAdder;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringProcessor stringProcessor = new StringProcessor(input);
+        StringProcessResult stringProcessResult = stringProcessor.getSplitToken();
+        int[] numbers = stringProcessResult.split();
+        System.out.printf("결과: %d\n", NumAdder.add(numbers));
+    }
+}
