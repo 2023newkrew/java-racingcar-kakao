340ba5592a9227fc3690d029855fed36c9bbdaa9
siryeong <siryeongchoi@gmail.com>
2022-12-26 16:25:10 +0900

    feat(StrCal): 음수 처리
    음수 포함된 문자열 처리
     - 음수가 포함된 경우 생성자에서 RuntimeException 던짐

--- src/main/java/calculator/StrCal.java
+++ src/main/java/calculator/StrCal.java
@@ -5,25 +5,29 @@ public class StrCal {
     private String regex = ",|:";
 
     public StrCal(String str) {
+        if (str.contains("-"))
+            throw new RuntimeException("negative value passed");
         this.str = str;
     }
 
     public StrCal(String del, String str) {
+        if (!del.contains("-") && str.contains("-"))
+            throw new RuntimeException("negative value passed");
+
         del = del.substring(2);
-        for(int i = 0; i < del.length(); i++)
+        for (int i = 0; i < del.length(); i++)
             regex += "|\\" + del.charAt(i);
 
         this.str = str;
     }
 
     public int calculate() {
-        if(str == null || str.length() == 0)
+        if (str == null || str.length() == 0)
             return 0;
 
         int sum = 0;
         String[] numbers = str.split(regex);
-        for(int i = 0; i < numbers.length; i++)
-            sum += Integer.parseInt(numbers[i]);
+        for (String number : numbers) sum += Integer.parseInt(number);
 
         return sum;
     }
