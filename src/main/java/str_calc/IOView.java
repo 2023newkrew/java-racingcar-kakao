28367b29804056988ae5dbe09eb6d0fd550a4498
Eithea <oguraff94@gmail.com>
2022-12-26 17:50:18 +0900

    feat: 계산기 Controller 구현

--- src/main/java/str_calc/IOView.java
+++ src/main/java/str_calc/IOView.java
@@ -1,23 +1,20 @@
 package str_calc;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class IOView {
-    private BufferedReader br;
+    private Scanner scanner;
 
     public IOView() {
-        this.br = new BufferedReader(new InputStreamReader(System.in));
+        this.scanner = new Scanner(System.in);
     }
 
     public String inputText() {
         System.out.print("텍스트를 입력해주세요 : ");
-        String text = null;
-        try {
-            text = br.readLine();
-        } catch (Exception e) {
-            e.printStackTrace();
+        String text = scanner.nextLine();
+        if(text.matches("//.*")) {
+            text += "\n";
+            text += scanner.nextLine();
         }
         return text;
     }
