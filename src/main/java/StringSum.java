87fe1f3c17efdf4c98f4170caf45db88ae6f5d0d
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-26 14:42:23 +0900

    feat: 생성자 작성

---
+++ src/main/java/StringSum.java
@@ -0,0 +1,11 @@
+public class StringSum {
+
+    private String formula;
+    private String delimiter;
+
+    public StringSum(String formula) {
+        this.formula = formula;
+        delimiter = ",|:";
+    }
+
+}
