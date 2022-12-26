f24b1cbb9361b27c26a64625f476dc4527508319
kjaehyeon <jacob9404@gmail.com>
2022-12-26 13:54:04 +0900

    feat: Calculator 클래스 생성 & 요구사항 1 작성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calculator {
+    public Integer calculate(String expression) {
+        if (expression.isBlank()) {
+            return 0;
+        }
+        return 1;
+    }
+}
