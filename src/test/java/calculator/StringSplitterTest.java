44a7e680190450fe8bc9659b1c5b61b84bd2410c
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-26 17:48:48 +0900

    test: StringSplitter 테스트 케이스 추가
    - 기본 구분자 ";" 추가
    - null, 공백 처리 추가
    - convertEmptyToZero(): 공백 문자열 0으로 변환

--- src/test/java/calculator/StringSplitterTest.java
+++ src/test/java/calculator/StringSplitterTest.java
@@ -35,16 +35,39 @@ class StringSplitterTest {
     }
 
     @Test
+    void convertEmptyToZero() {
+        String[] arr = {"1","","2"};
+        StringSplitter stringSplitter = new StringSplitter("");
+        assertArrayEquals(new String[] {"1","0","2"}, stringSplitter.convertEmptyToZero(arr));
+    }
+
+    @Test
     void split() {
-        String str = "//;;\n6;;7;;34;;45";
-        String str2 = "6,7,34,45";
+        String str = "//;;\n6;;0;;34;;45";
+        String str2 = "6,,34;45";
+        String str3 = "5";
 
         StringSplitter stringSplitter = new StringSplitter(str);
         StringSplitter stringSplitter2 = new StringSplitter(str2);
+        StringSplitter stringSplitter3 = new StringSplitter(str3);
 
-        String[] expectedResult = {"6", "7", "34", "45"};
+        String[] expectedResult = {"6", "0", "34", "45"};
 
         assertArrayEquals(expectedResult, stringSplitter.split());
         assertArrayEquals(expectedResult, stringSplitter2.split());
+        assertArrayEquals(new String[]{"5"}, stringSplitter3.split());
+    }
+
+    @Test
+    void split_empty() {
+        String str1 = "";
+        String str2 = null;
+        String[] result = {"0"};
+
+        StringSplitter stringSplitter1 = new StringSplitter(str1);
+        StringSplitter stringSplitter2 = new StringSplitter(str2);
+
+        assertArrayEquals(result, stringSplitter1.split());
+        assertArrayEquals(result, stringSplitter2.split());
     }
 }
