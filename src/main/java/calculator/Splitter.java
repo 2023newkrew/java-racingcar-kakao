7131648fe7c1d07dd86a7a497bd911109d942b0e
kjaehyeon <jacob9404@gmail.com>
2022-12-26 15:01:41 +0900

    feat: 요구사항5 구현

--- src/main/java/calculator/Splitter.java
+++ src/main/java/calculator/Splitter.java
@@ -2,12 +2,24 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Splitter {
 
     public List<Integer> split(String input){
-        return Arrays.stream(input.split("[,;]"))
+        String delimiters = "[,;";
+        Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(input);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            delimiters += customDelimiter;
+            input = input.split("\n")[1];
+        }
+        delimiters += "]";
+
+        return Arrays.stream(input.split(delimiters))
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
