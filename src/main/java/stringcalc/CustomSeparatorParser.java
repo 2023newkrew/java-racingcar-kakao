0dd4f8da9de294b38d18d418b8fa2e7b7ee38cd7
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-26 16:23:36 +0900

    feat: 커스텀 구분자 예외 사항 추가

--- src/main/java/stringcalc/CustomSeparatorParser.java
+++ src/main/java/stringcalc/CustomSeparatorParser.java
@@ -2,32 +2,23 @@ package stringcalc;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CustomSeparatorParser {
+
     public List<String> parse(String input) {
-        Pattern pattern = Pattern.compile("(?<=//)(.*?)(?=\n)");
-        Matcher matcher = pattern.matcher(input);
+        String[] split = input.split("\n");
         List<String> separators = new ArrayList<>();
-        while (matcher.find()) {
-            separators.add(matcher.group(1));
+        for (int idx = 0; idx < split.length - 1; idx++) {
+            validateSeparatorFormat(split[idx]);
+            separators.add(split[idx].substring(2));
         }
-        validateSeparatorLength(separators);
         return separators;
     }
 
-    private void validateSeparatorLength(List<String> separators){
-        for (String separator : separators) {
-            checkLengthOne(separator);
-        }
-    }
-
-    private void checkLengthOne(String separator){
-        if (separator.length() != 1) {
+    private void validateSeparatorFormat(String separator){
+        if (separator.length() != 3 || !separator.startsWith("//")) {
             throw new RuntimeException();
         }
     }
-
 
 }
