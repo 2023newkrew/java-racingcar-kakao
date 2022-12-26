981994e0cd555bbbb262e260ba86fef00b10ffef
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-26 14:42:47 +0900

    feat: 커스텀 구분자 추출 함수 작성

--- src/main/java/StringSum.java
+++ src/main/java/StringSum.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringSum {
 
     private String formula;
@@ -6,6 +9,18 @@ public class StringSum {
     public StringSum(String formula) {
         this.formula = formula;
         delimiter = ",|:";
+    }
+
+    public String customDelimiter() {
+        // java.util.regex 패키지의 Matcher, Pattern import
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(formula);
+        String customDelimiter = null;
+        if (m.find()) {
+            customDelimiter = m.group(1);
+            formula =  m.group(2);
+            delimiter += "|" + customDelimiter;
+        }
+        return customDelimiter;
     }
 
 }
