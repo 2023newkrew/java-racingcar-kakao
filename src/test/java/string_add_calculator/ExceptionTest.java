a48cd022554f3b786849b537e764639925952d2c
gkwodn1044 <gkwodn1044@gmail.com>
2022-12-26 15:40:49 +0900

    feat : ExceptionTest 양의 정수 검증

---
+++ src/test/java/string_add_calculator/ExceptionTest.java
@@ -0,0 +1,21 @@
+package string_add_calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExceptionTest {
+    @Test
+    void exceptionTest(){
+        Calculator calculator = new Calculator(new String[] {"-1", "2", "3"});
+        String str;
+        try {
+            calculator.isInteger();
+            str = "성공";
+        }catch (RuntimeException e){
+            str = "실패";
+        }
+
+        assertEquals(str, "실패");
+    }
+}
