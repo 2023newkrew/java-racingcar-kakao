1941940d9ff3d3e8a1e22846b6e898c48224aba8
정핸모 <gusah009@naver.com>
2022-12-26 18:01:44 +0900

    test: 문자열 목록을 숫자로 변환해서 더하는 기능 테스트 코드 추가

--- src/test/java/stringaccumulator/StringAccumulatorTest.java
+++ src/test/java/stringaccumulator/StringAccumulatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,6 +38,43 @@ class StringAccumulatorTest {
             assertThatThrownBy(() -> {
                 StringAccumulator.from("//.\123");
             }).isInstanceOf(RuntimeException.class);
+        }
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class accumulate {
+        @ParameterizedTest
+        @MethodSource
+        void should_returnSumOfTokens_when_validTokens(StringAccumulator accumulator, int expected) {
+            long result = accumulator.accumulate();
+            assertThat(result).isEqualTo(expected);
+        }
+
+        Stream<Arguments> should_returnSumOfTokens_when_validTokens() {
+            return Stream.of(
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of(""))), 0),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("1", "2", "3"))), 6),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("1", " 2", " 3"))), 6),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("1", "2 ", "3 "))), 6),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("1", "", "2", "3"))), 6)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource
+        void should_throwException_when_newlineNotExists(StringAccumulator accumulator) {
+            assertThatThrownBy(accumulator::accumulate)
+                    .isInstanceOf(RuntimeException.class);
+        }
+
+        Stream<Arguments> should_throwException_when_newlineNotExists() {
+            return Stream.of(
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("-1", "1")))),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("1 2", "3")))),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of("asd")))),
+                    Arguments.of(StringAccumulator.from(String.join(",", List.of(String.valueOf(Long.MAX_VALUE)) + "123")))
+            );
         }
     }
 }
