9d4e0d08f7077ab5c165004c7ea0b1f5e95a971d
kjaehyeon <jacob9404@gmail.com>
2022-12-26 14:45:39 +0900

    refactor: Calculator 요구사항 2 리팩토링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,9 +2,14 @@ package calculator;
 
 public class Calculator {
     public Integer calculate(String expression) {
-        if (expression.isBlank()) {
+        if (expression.isBlank()){
             return 0;
         }
+        if (expression.length() == 1 && Character.isDigit(expression.charAt(0))){
+            return Integer.parseInt(expression);
+        }
+
         return 1;
     }
+
 }
