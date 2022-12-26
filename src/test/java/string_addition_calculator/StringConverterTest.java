46b61672ca25e749c07f7488efdccf19f111eee2
kakao <coolofficials@gmail.com>
2022-12-26 15:20:05 +0900

    refactor: 테스트 공통 할당 로직 추출

--- src/test/java/string_addition_calculator/StringConverterTest.java
+++ src/test/java/string_addition_calculator/StringConverterTest.java
@@ -1,5 +1,6 @@
 package string_addition_calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,30 +11,33 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOf
 // "-1" -> -1
 
 public class StringConverterTest {
+    StringConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new StringConverter();
+    }
+
     @Test
     void convert_single_digit() {
-        StringConverter converter = new StringConverter();
         converter.convertString("1");
         assertThat(converter.getResult()).isEqualTo(1);
     }
 
     @Test
     void convert_multiple_digits() {
-        StringConverter converter = new StringConverter();
         converter.convertString("12");
         assertThat(converter.getResult()).isEqualTo(12);
     }
 
     @Test
     void convert_negative() {
-        StringConverter converter = new StringConverter();
         converter.convertString("-1");
         assertThat(converter.getResult()).isEqualTo(-1);
     }
 
     @Test
     void convert_not_integer() {
-        StringConverter converter = new StringConverter();
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> converter.convertString("hello")).withMessage("정수로 변환할 수 없는 문자열입니다.");
     }
