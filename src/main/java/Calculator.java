da28c0e77bb4d9495838efb3daed2ac5bbb5ee2a
park0503 <hooo0503@gmail.com>
2022-12-26 14:30:32 +0900

    feat: 복수 숫자 계산 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Calculator {
     public int calculate(String input) {
         if (Objects.isNull(input) || input.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(input);
+        String[] numbers = input.split(",");
+        Optional<Integer> result = Arrays.stream(numbers).map(this::parseToInt).reduce(Integer::sum);
+        return result.orElse(-1);
+    }
+
+    private int parseToInt(String next) {
+        return Integer.parseInt(next);
     }
 }
