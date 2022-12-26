d181e9a0470e06adba7933a5ec4d18327278fd09
Nelfunction <oat641@gmail.com>
2022-12-26 17:54:40 +0900

    test: complete perfect test code

--- src/test/java/StringAdditionTest.java
+++ src/test/java/StringAdditionTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +9,8 @@ public class StringAdditionTest {
     @DisplayName("기본 구분자 - 정답")
     void defaultDelimitersValid() {
         String testString = "1,2:3";
-        Calculator cal = new Calculator(testString);
-        String[] result = del.separate(testString);
+        Parser calc = new Parser(testString);
+        String[] result = calc.parse();
         assertArrayEquals(result, new String[]{"1", "2", "3"});
     }
 
@@ -19,7 +18,43 @@ public class StringAdditionTest {
     @DisplayName("커스텀 구분자 분리 - 정답")
     void separateCustomDelimiter() {
         String testString = "//;\n1;2;3,4:5";
-        Calculator calc = new Calculator(testString);
+        Parser calc = new Parser(testString);
+        String[] result = calc.parse();
+        assertArrayEquals(result, new String[]{"1", "2", "3", "4", "5"});
     }
 
+    @Test
+    @DisplayName("허용되지 않은 특수문자 감지")
+    void detectNegative() {
+        String[] testStrings = new String[]{"#", "2", "3", "4", "5"};
+        assertThrowsExactly(RuntimeException.class, ()->Validator.validate(testStrings));
+    }
+
+    @Test
+    @DisplayName("음수 감지")
+    void detectNonNumber() {
+        String[] testStrings = new String[]{"-1", "2", "3", "4", "0"};
+        assertThrowsExactly(RuntimeException.class, ()->Validator.validate(testStrings));
+    }
+
+    @Test
+    @DisplayName("문자 배열 숫자 변환 후 덧셈")
+    void convertToNumbers() {
+        String[] testStrings = new String[]{"1", "2", "3", "4", "5"};
+        Calculator calc = new Calculator();
+        assertEquals(calc.sumAll(testStrings), 15);
+    }
+
+    @Test
+    @DisplayName("최종 덧셈 결과")
+    void calculateResult() {
+        String testString = "//;\n1;2;3,4:5";
+        Parser parser = new Parser(testString);
+        String[] parseResult = parser.parse();
+
+        Calculator calc = new Calculator();
+        int result = calc.sumAll(parseResult);
+
+        assertEquals(result, 15);
+    }
 }
