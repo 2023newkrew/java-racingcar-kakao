f81265317089c17afdfe901913b413596274ff27
yooj929 <jang0929@naver.com>
2022-12-26 13:47:22 +0900

    feat: null이면 0을 리턴한다.

--- src/main/java/StringAdder.java
+++ src/main/java/StringAdder.java
@@ -1,6 +1,10 @@
 public class StringAdder {
     public int add(String text) {
         int number = -1;
+
+        if(text == null){
+            return 0;
+        }
         try{
             number = Integer.parseInt(text);
         }
