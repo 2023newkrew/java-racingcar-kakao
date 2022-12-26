e8b3c3aceb43bf2abfaba7a11664958d2413baf1
yooj929 <jang0929@naver.com>
2022-12-26 16:35:43 +0900

    feat: move 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,17 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void move(int seed){
+        if(seed>3){
+            distance++;
+        }
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,2 +1,33 @@
-package racingcar;public class CarTest {
+package racingcar;
+
+import java.lang.reflect.Field;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    private static Field distance;
+
+    @BeforeAll
+    public static void setUp() throws NoSuchFieldException {
+        distance = Car.class.getDeclaredField("distance");
+        distance.setAccessible(true);
+    }
+
+    @Test
+    public void moveTest() throws IllegalAccessException {
+        Car car = new Car("abc");
+        car.move(5);
+        int dist = (int) distance.get(car);
+        Assertions.assertThat(dist).isEqualTo(1);
+    }
+
+    @Test
+    public void doNotMoveTest() throws IllegalAccessException {
+        Car car = new Car("abc");
+        car.move(2);
+        int dist = (int) distance.get(car);
+        Assertions.assertThat(dist).isEqualTo(0);
+    }
 }
