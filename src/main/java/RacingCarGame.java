d0c56123c7863a41ab069577326ee66e3d721417
aaron.j <aaron.j@kakaocorp.com>
2022-12-26 15:13:01 +0900

    feat: 숫자 하나를 문자열 입력 시 해당 숫자를 반환하는 기능 구현

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -1,6 +1,13 @@
+import java.util.Objects;
+
 public class RacingCarGame {
 
     public int play(String input) {
+        if (!Objects.isNull(input) && input.length() == 1) {
+            return Integer.parseInt(input);
+        }
+
         return 0;
     }
+
 }
--- src/test/java/RacingcarTest.java
+++ src/test/java/RacingcarTest.java
@@ -14,13 +14,35 @@ public class RacingcarTest {
         int result = racingCarGame.play("");
         assertThat(result).isEqualTo(0);
     }
-    
+
     @Test
     @DisplayName("null 값을 입력할 경우 0을 반환한다.")
     void returnZeroIfNull() {
         RacingCarGame racingCarGame = new RacingCarGame();
-        
+
         int result = racingCarGame.play(null);
         assertThat(result).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    void returnOneIfNumberCharacter() {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        String input = "1";
+        int expectedResult = Integer.parseInt(input);
+
+        int result = racingCarGame.play(input);
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    @DisplayName("")
+    void throwExceptionIfCharacterIsNotNumber() {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        String input = "-";
+        
+        assertThatThrownBy(() -> racingCarGame.play(input))
+                .isInstanceOf(RuntimeException.class);
+    }
+
 }
