cb4102581123339befcd5c76b3916fe0865b0e91
park0503 <hooo0503@gmail.com>
2022-12-26 16:29:29 +0900

    feat: 자동차 이동 기능 구현
    - power가 4 이상일 시 이동하는 메소드 구현
    - 자동차의 현재 상태(위치) 출력 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,21 @@
 package racingcar;
 
 public class Car {
+    private final String name;
+    private int position = 1;
+
     public Car(String name) {
+        this.name = name;
     }
 
     public void moveWithPower(int power) {
+        if (power > 3) {
+            this.position++;
+        }
     }
 
     public String getCurrentStatus() {
-        return null;
+        return this.name + " : "
+                + "-".repeat(Math.max(0, this.position));
     }
 }
