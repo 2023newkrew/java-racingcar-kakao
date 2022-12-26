f5b2d02e064042c598b1aa8a4a1ec02a6400158c
GrasshopperBears <starday456@naver.com>
2022-12-26 16:14:58 +0900

    refactor: getStringProcessResult 반환값 즉시 사용

--- src/main/java/stringAdder/Main.java
+++ src/main/java/stringAdder/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("계산식을 입력하세요: ");
         String input = sc.nextLine();
         StringProcessor stringProcessor = new StringProcessor(input);
-        StringProcessResult stringProcessResult = stringProcessor.getStringProcessResult();
-        int[] numbers = stringProcessResult.split();
+        int[] numbers = stringProcessor.getStringProcessResult().split();
         System.out.printf("결과: %d\n", NumAdder.add(numbers));
     }
 }
