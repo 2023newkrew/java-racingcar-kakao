873734387eb548c583c3fefe1a42b32a0fc3f199
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-26 16:45:52 +0900

    feat: Main 클래스 문자열 입력 작성

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String inputString() {
+        System.out.print("문자열을 입력해주세요. : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if(input.startsWith("//")) {
+            String input_2 = scanner.nextLine();
+            input += "\n" + input_2;
+        }
+        return input;
+    }
+}
