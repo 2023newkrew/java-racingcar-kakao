8f5649d6531e27d017e97b1fc6b24d78c391e92b
kjaehyeon <jacob9404@gmail.com>
2022-12-27 11:24:16 +0900

    fix: splitter 접근 제한자 수정

--- src/main/java/racing/service/RacingSplitter.java
+++ src/main/java/racing/service/RacingSplitter.java
@@ -8,16 +8,16 @@ import java.util.List;
 public class RacingSplitter {
     private final RacingValidator racingValidator;
 
-    RacingSplitter() {
+    public RacingSplitter() {
         racingValidator = new RacingValidator();
     }
 
-    List<Car> split(String carNames) {
+    public List<Car> split(String carNames) {
         List<Car> ret = new ArrayList<>();
         for (String carName: carNames.split(",")) {
             carName = carName.trim();
             racingValidator.carNameValidate(carName);
-            ret.add(new Car(carName, 0));
+            ret.add(new Car(carName, 1));
         }
         return ret;
     }
