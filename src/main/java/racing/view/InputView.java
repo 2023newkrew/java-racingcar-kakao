415a4fdb6d1d204c4a74debae5543d0b24df7542
kyne.k <dd2307@snu.ac.kr>
2022-12-27 16:36:33 +0900

    feat(racing): InputView, OutputView 클래스 생성
    - InputView 클래스 생성
      - readCarNames() 메서드
      - readGameRepeat() 메서드
    - OutputView 클래스 생성
      - printWinner() 메서드
      - printResult() 메서드

---
+++ src/main/java/racing/view/InputView.java
@@ -0,0 +1,13 @@
+package racing.view;
+
+import java.util.List;
+
+public class InputView {
+    public List<String> readCarNames() {
+        return null;
+    }
+
+    public int readGameRepeat() {
+        return -1;
+    }
+}
---
+++ src/main/java/racing/view/OutputView.java
@@ -0,0 +1,11 @@
+package racing.view;
+
+import java.util.List;
+
+public class OutputView {
+    public void printWinner(List<String> winners) {
+    }
+
+    public void printResult(List<String> status) {
+    }
+}
