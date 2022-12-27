6d50c49e00dbf84cebc0c9f259601c0880ede078
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-27 11:44:56 +0900

    feat: 이름 조건 제한 코드 작성

--- src/main/java/CarRacing/Racing.java
+++ src/main/java/CarRacing/Racing.java
@@ -4,4 +4,10 @@ public class Racing {
     public String[] nameSplit(String inputString) {
         return inputString.split(",");
     }
+
+    public void nameException(String name) {
+        if (name.length() > 5) {
+            throw new RuntimeException("다섯 글자 이상입니다.");
+        }
+    }
 }
