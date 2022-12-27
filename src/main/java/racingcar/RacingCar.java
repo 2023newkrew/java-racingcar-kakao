60f7283f81b8bc72f7f4e3331a1aa6f8de71b345
HONGMINJU <mhong0800@gmail.com>
2022-12-27 16:18:10 +0900

    feat: 자동차 객체 추가

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+public class RacingCar {
+
+    private final String name;
+    private int distance;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.distance = 1;
+    }
+
+    public void move(boolean isMove) {
+        if (!isMove) {
+            return;
+        }
+        this.distance++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name).append(" : ");
+        for (int step = 0; step < distance; step++) {
+            stringBuilder.append('-');
+        }
+        return stringBuilder.toString();
+    }
+}
