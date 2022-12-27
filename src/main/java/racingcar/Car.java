2ce29fdad3eadb0ed4517c72d5af3bee2e9d7a77
SJGeeKo <sungjun.gee.ko@gmail.com>
2022-12-27 13:20:07 +0900

    feat: Car 기능 구현
    - Car(): Car 생성시 이름과 거리 초기화
    - pickNumber(): 0부터 9사이의 숫자를 하나 선택
    - move(): pickNumber()의 결과가 4 이상일 경우 거리 1 증가
    - toString(): 디버깅을 위한 Car 정보 문자열 반환

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public int pickNumber() {
+        return (int) (Math.random() * 10);
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        if (pickNumber() >= 4) {
+            distance++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("차 이름: %s, 거리: %d", name, distance);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
