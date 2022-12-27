f7bb5f74c5c2209ca262a3cd8b36ca3a94e39df3
kyne.k <dd2307@snu.ac.kr>
2022-12-27 17:01:16 +0900

    feat(racing): InputView 클래스 구현
    - InputView 클래스 구현
      - readCarNames() 메서드
      - readGameRepeat() 메서드

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,13 +1,37 @@
 package racing.view;
 
 import java.util.List;
+import java.util.Scanner;
+import racing.utils.InputValidator;
 
 public class InputView {
+    private final Scanner scanner;
+    private final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final String INPUT_REPEAT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final InputValidator validator;
+
+    public InputView() {
+        scanner = new Scanner(System.in);
+        validator = new InputValidator();
+    }
+
     public List<String> readCarNames() {
-        return null;
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        try {
+            return validator.validateCarNames(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readCarNames();
+        }
     }
 
     public int readGameRepeat() {
-        return -1;
+        System.out.println(INPUT_REPEAT_MESSAGE);
+        try {
+            return validator.validateGameRepeat(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readGameRepeat();
+        }
     }
 }
