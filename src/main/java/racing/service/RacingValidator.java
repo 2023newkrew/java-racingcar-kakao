fd840865c27f3f67102e381752bbbd41eb6c64bd
kjaehyeon <jacob9404@gmail.com>
2022-12-27 12:02:12 +0900

    feat: RacingValidator에 attemptValidate 구현

--- src/main/java/racing/service/RacingValidator.java
+++ src/main/java/racing/service/RacingValidator.java
@@ -1,11 +1,15 @@
 package racing.service;
 
-import racing.exception.CarNameInvalidException;
+import racing.exception.RacingException;
 import racing.exception.ErrorCode;
 
 public class RacingValidator {
     public void carNameValidate(String carName){
-        if(carName.isBlank()) throw new CarNameInvalidException(ErrorCode.EMPTY_CAR_NAME);
-        if(carName.length() > 5) throw new CarNameInvalidException(ErrorCode.TOO_LONG_CAR_NAME);
+        if(carName.isBlank()) throw new RacingException(ErrorCode.EMPTY_CAR_NAME);
+        if(carName.length() > 5) throw new RacingException(ErrorCode.TOO_LONG_CAR_NAME);
+    }
+
+    public void attemptValidate(Integer attempt){
+        if(attempt < 0) throw new RacingException(ErrorCode.NEGATIVE_ATTEMPT);
     }
 }
