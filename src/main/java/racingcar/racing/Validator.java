3a81f625ab0879e9eb30ebae163e1af387c84cbe
jgus52 <srraight@gmail.com>
2022-12-27 17:24:52 +0900

    Refactor: 이름 길이 상수로 변경

--- src/main/java/racingcar/racing/Validator.java
+++ src/main/java/racingcar/racing/Validator.java
@@ -3,15 +3,15 @@ package racingcar.racing;
 import java.util.List;
 
 public class Validator {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 0;
+
 
     public static boolean isValidSingleName(String name) {
-        if (name.length() > 5 || name.length() == 0) {
+        if (name.length() > MAX_NAME_LENGTH || name.length() <= MIN_NAME_LENGTH) {
             return false;
         }
-        if (name.contains(" ")) {
-            return false;
-        }
-        return true;
+        return !name.contains(" ");
     }
 
     public static boolean isValidNames(List<String> names) {
