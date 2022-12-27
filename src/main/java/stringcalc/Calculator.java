4f899f917885f7869aa059c09dc83ed00f8ef37d
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-27 11:05:07 +0900

    feat: 문자열 입력을 받아 parser를 호출한 뒤 숫자 합을 구하는 기능 추가

--- src/main/java/stringcalc/Calculator.java
+++ src/main/java/stringcalc/Calculator.java
@@ -3,7 +3,22 @@ package stringcalc;
 import java.util.List;
 
 public class Calculator {
-    public Long sum(List<Long> numbers) {
+
+    private final StringNumberParser stringNumberParser;
+    private final CustomSeparatorParser customSeparatorParser;
+
+    public Calculator() {
+        stringNumberParser = new StringNumberParser();
+        customSeparatorParser = new CustomSeparatorParser();
+    }
+
+    private Long sum(List<Long> numbers) {
         return numbers.stream().reduce(0L, Long::sum);
+    }
+
+    public Long calculate(String input) {
+        String customSeparator = customSeparatorParser.parse(input);
+        List<Long> numbers = stringNumberParser.parse(input, customSeparator);
+        return sum(numbers);
     }
 }
