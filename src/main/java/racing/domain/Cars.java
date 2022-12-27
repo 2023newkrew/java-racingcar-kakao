07d5d186d479e2ff4caf3c358058b63e99946307
kyne.k <dd2307@snu.ac.kr>
2022-12-27 15:54:42 +0900

    feat(racing): 기능 5 구현
    - Cars 클래스가 List<Car>을 래핑하도록 구현
    - getWinners() 메서드 구현

---
+++ src/main/java/racing/domain/Cars.java
@@ -0,0 +1,40 @@
+package racing.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+    private final int length;
+
+    public Cars(List<String> carNames) {
+        cars = carNames.stream()
+                .map(name -> new Car(name))
+                .collect(Collectors.toList());
+        length = cars.size();
+    }
+
+    public void play(List<Double> randomNumbers) {
+        if (randomNumbers.size() != length) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < length; i++) {
+            cars.get(i).move(randomNumbers.get(i));
+        }
+    }
+
+    public List<String> getWinners() {
+        Collections.sort(cars);
+        Car winnerCar = cars.get(length - 1);
+        List<String> winners = cars.stream()
+                .filter(car -> car.compareTo(winnerCar) == 0)
+                .map(car -> car.name)
+                .collect(Collectors.toList());
+        return winners;
+    }
+
+    public List<String> getStatus() {
+        return null;
+    }
+}
