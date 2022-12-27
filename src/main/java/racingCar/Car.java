74f3fc46b65aef175070d193d08fbee28a62a663
GrasshopperBears <starday456@naver.com>
2022-12-27 11:47:01 +0900

    feat: 유저에게 입력을 받아 게임 실행하는 main 함수 구현

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -15,6 +15,10 @@ public class Car {
         return position;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void move() {
         position++;
     }
--- src/main/java/racingCar/Main.java
+++ src/main/java/racingCar/Main.java
@@ -1,5 +1,7 @@
 package racingCar;
 
+import java.util.Scanner;
+
 public class Main {
     public static Car[] namesToCars(String input) {
         String[] names = input.split(",");
@@ -8,5 +10,28 @@ public class Main {
             cars[i] = new Car(names[i]);
         }
         return cars;
+    }
+
+    public static String carsToNames(Car[] cars) {
+        StringBuilder sb = new StringBuilder();
+        for (Car car: cars) {
+            sb.append(car.getName()).append(',');
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNameString = sc.nextLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int totalTurns = sc.nextInt();
+        System.out.println("\n실행 결과");
+        Car[] cars = Main.namesToCars(carNameString);
+        Game game = new Game(totalTurns, cars);
+        // 출력
+        Car[] winners = game.findWinners();
+        System.out.printf("%s가 최종 우승했습니다.\n", Main.carsToNames(winners));
     }
 }
