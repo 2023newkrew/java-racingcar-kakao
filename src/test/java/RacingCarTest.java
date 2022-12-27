f678ba616ce4c6454cc21b38e2b55ef818ea68b7
Nelfunction <oat641@gmail.com>
2022-12-27 13:50:37 +0900

    test: generate cars

--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -1,7 +1,20 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racing_car.model.Car;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RacingCarTest {
+
+    @Test
+    @DisplayName("제공된 문자열로 자동차 배열 생성")
+    void separateByComma() {
+        String testNames = "pobi,crong,honux";
+        Car[] cars = Car.from(testNames);
+        assertArrayEquals(cars, new Car[] {
+                new Car("pobi"),
+                new Car("crong"),
+                new Car("honux")
+        });
+    }
 }
