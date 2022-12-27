71dd5983dd62129b0a9357fcd5bc2dd1bc079ce7
park0503 <hooo0503@gmail.com>
2022-12-27 14:39:21 +0900

    feat: 실행 횟수 파싱 기능 구현
    - 숫자인지 검증
    - 양수인지 검증

--- src/main/java/racingcar/InputParser.java
+++ src/main/java/racingcar/InputParser.java
@@ -3,10 +3,26 @@ package racingcar;
 public class InputParser {
 
     public String[] splitByComma(String input) {
+        String[] result = input.split(",");
+        if (result.length < 2) {
+            throw new RuntimeException("경주에는 최소 2대 이상의 자동차가 필요합니다.");
+        }
         return input.split(",");
     }
 
     public int parseToInt(String input) {
-        return Integer.parseInt(input);
+        try {
+            int number = Integer.parseInt(input);
+            checkPositive(number);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("1 이상의 숫자만 입력해야합니다.");
+        }
+    }
+
+    private void checkPositive(int number) {
+        if (number <= 0) {
+            throw new NumberFormatException();
+        }
     }
 }
