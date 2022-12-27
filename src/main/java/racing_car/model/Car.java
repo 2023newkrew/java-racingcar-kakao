881f8fcabc61a98474d6123f5bbab2172cbc8440
Nelfunction <oat641@gmail.com>
2022-12-27 13:52:25 +0900

    feat: generate cars

---
+++ src/main/java/racing_car/model/Car.java
@@ -0,0 +1,31 @@
+package racing_car.model;
+
+import java.util.Objects;
+
+public class Car {
+
+    private final String name;
+
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public static Car[] from(String names){
+        String[] separatedNames = names.split(",");
+        Car[] cars = new Car[separatedNames.length];
+        for (int i = 0; i < separatedNames.length; i++) {
+            cars[i] = new Car(separatedNames[i]);
+        }
+        return cars;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return distance == car.distance && Objects.equals(name, car.name);
+    }
+}
