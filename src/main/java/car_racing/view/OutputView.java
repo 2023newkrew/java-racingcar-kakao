c10a0951ce2dfef01b659748eff7943729ec0775
BaeRoNuI <xogh20321@gmail.com>
2022-12-28 00:18:41 +0900

    rafactor: remove meaningless overheads

--- src/main/java/car_racing/view/OutputView.java
+++ src/main/java/car_racing/view/OutputView.java
@@ -16,20 +16,17 @@ public class OutputView {
     }
 
     public static void printCurrentStatus(List<RacingPlayer> racingPlayers) {
-        for (RacingPlayer racingPlayer :
-                racingPlayers) {
-            System.out.println(racingPlayer.toString());
-        }
+        racingPlayers.forEach((player) -> System.out.println(player.toString()));
         System.out.println();
     }
 
-    public static void printWinners(RacingPlayer[] winners) {
+    public static void printWinners(List<RacingPlayer> winners) {
         String concatenatedName = getConcatenatedNameFromArray(winners);
         System.out.println(concatenatedName + "가 최종 우승했습니다.");
     }
 
-    private static String getConcatenatedNameFromArray(RacingPlayer[] winners) {
-        return Arrays.stream(winners)
+    private static String getConcatenatedNameFromArray(List<RacingPlayer> winners) {
+        return winners.stream()
                 .map(RacingPlayer::getName)
                 .collect(Collectors.joining(", "));
     }
