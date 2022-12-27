742c417a27179e081ad111e51b8652cb33d6277b
park0503 <hooo0503@gmail.com>
2022-12-27 11:12:51 +0900

    test: 자동차 생성 테스트 작성
    - 이름이 공백으로만 이루어져 있을 시 Exception 발생
    - 이름이 5글자가 넘어갈 시 Exception 발생

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,10 +2,27 @@ package racingcar;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
     private final Car car =  new Car("pobi");
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "jaws", "jayde"})
+    public void 자동차_생성(String input) {
+        Car newCar = new Car(input);
+        Assertions.assertThat(newCar.getCurrentStatus()).isEqualTo(input + " : -");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"   ", "jayden"})
+    public void 잘못된_이름의_자동차_생성(String input) {
+        Assertions.assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+            Car newCar = new Car(input);
+        }).withMessage("잘못된 자동차 이름입니다.");
+    }
 
     @Test
     public void 랜덤값이_4이상이면_전진() {
--- src/test/java/racingcar/RefereeTest.java
+++ src/test/java/racingcar/RefereeTest.java
@@ -13,10 +13,10 @@ public class RefereeTest {
     public void 우승자를_가린다() {
         Car car1 = new Car("pobi", 3);
         Car car2 = new Car("jaws", 5);
-        Car car3 = new Car("jayden", 5);
+        Car car3 = new Car("jayde", 5);
         List<Car> cars = Arrays.asList(car1, car2, car3);
         List<String> winners = referee.judgeWinner(cars);
-        Assertions.assertThat(winners).containsExactly("jaws", "jayden");
+        Assertions.assertThat(winners).containsExactly("jaws", "jayde");
     }
 
 
