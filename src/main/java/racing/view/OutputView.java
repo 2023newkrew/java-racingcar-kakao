9ec018e9db5dbbd2c37b478e7dbc7c8498d5500c
kyne.k <dd2307@snu.ac.kr>
2022-12-27 16:59:55 +0900

    feat(racing): OutputView 구현
    - OutputView 클래스 구현
      - printWinner() 메서드
      - printResult() 메서드

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -3,9 +3,14 @@ package racing.view;
 import java.util.List;
 
 public class OutputView {
-    public void printWinner(List<String> winners) {
+    private final String WINNER_MESSAGE_FORMAT = "%s가 최종 우승했습니다.";
+    public void printResult(List<String> status) {
+        status.stream().forEach(System.out::println);
+        System.out.println();
     }
 
-    public void printResult(List<String> status) {
+    public void printWinner(List<String> winners) {
+        String winnerString = String.join(", ", winners);
+        System.out.printf(WINNER_MESSAGE_FORMAT, winnerString);
     }
 }
