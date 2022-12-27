2f5cb8e0675eec75f2ac1b8e4ecc2647b09c5937
sean.m <sean.m@kakaocorp.com>
2022-12-27 14:12:32 +0900

    feat(레이싱카): 레이싱 클래스 구현
    레이싱 클래스 작성
     - 자동차를 입력받아 리스트에 추가하는 기능
     - 레이싱 진행하여 카운트 감소하는 기능
     - 레이싱 완료 확인 기능
     - 우승 차량 이름 리스트 반환 기능

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,48 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Racing {
+    private List<Car> cars = new ArrayList<>();
+    private int count;
+
+    public Racing(int count) {
+        this.count = count;
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public List<Car> getCarList() {
+        return cars;
+    }
+
+    public void doStep() {
+        cars.forEach(Car::forward);
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isFinished() {
+        return count == 0;
+    }
+
+    public List<String> winner() {
+        int maxDist = 0;
+        for(Car car : cars)
+            maxDist = Math.max(maxDist, car.getDistance());
+
+        List<String> result = new ArrayList<>();
+        final int finalMaxDist = maxDist;
+        Stream<Car> filtered = cars.stream().
+                filter(car ->  car.getDistance() == finalMaxDist);
+        filtered.forEach(car -> {result.add(car.getName());});
+        return result;
+    }
+}
