6e42d3794da0cdea5a6b80f8957fe5415077e0fa
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-27 13:47:39 +0900

    feat: 입력을 받을 때 "\n"를 처리하는 기능 추가

--- src/main/java/stringcalc/StringCalculatorApp.java
+++ src/main/java/stringcalc/StringCalculatorApp.java
@@ -6,7 +6,9 @@ public class StringCalculatorApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
+        input = input.replaceAll("\\\\n", "\n");
         StringCalculator stringCalculator = new StringCalculator();
         System.out.println(stringCalculator.calculate(input));
+        scanner.close();
     }
 }
