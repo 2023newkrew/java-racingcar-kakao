42cce432ff0db6cedf09490273008d24d9c061be
Neurumaru <ksj6659@gmail.com>
2022-12-27 14:55:06 +0900

    feat(Car): add position, toString

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -2,11 +2,18 @@ package racing;
 
 public class Car {
     private final String name;
+    private int position = 0;
 
     public Car(String name) {
         if (name.isEmpty() || name.length() > 5) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        String positionString = "-".repeat(position + 1);
+        return String.format("%s : %s", name, positionString);
     }
 }
