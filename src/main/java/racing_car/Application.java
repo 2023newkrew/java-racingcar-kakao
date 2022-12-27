3d09571b59d9f7da5b86ead8fe8279e65f1dcd17
Nelfunction <oat641@gmail.com>
2022-12-27 15:09:13 +0900

    fix: extract methods

--- src/main/java/racing_car/Application.java
+++ src/main/java/racing_car/Application.java
@@ -7,30 +7,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         GameControl gameControl = new GameControl();
+        Car[] cars = getCars(sc);
+        int iter = getIter(sc);
 
+        System.out.println("실행결과");
+        for (int i = 0; i < iter; i++) {
+            printGameProcess(gameControl, cars);
+        }
+        printGameResult(gameControl, cars);
+    }
+
+    private static Car[] getCars(Scanner sc) {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = sc.nextLine();
 
         Car[] cars = Car.from(input);
+        return cars;
+    }
 
+    private static int getIter(Scanner sc) {
         System.out.println("시도할 회수는 몇회인가요?");
         int iter = sc.nextInt();
+        return iter;
+    }
 
-        System.out.println("실행결과");
-        for (int i = 0; i < iter; i++) {
-            for (Car car: cars) {
-                gameControl.carControl(car);
-                System.out.print(car.getName() + " : " + car.showDistance() + "\n");
-            }
-            System.out.println();
-        }
-
+    private static void printGameResult(GameControl gameControl, Car[] cars) {
         Car[] winner = gameControl.getWinners(cars);
         String[] winnerNames = Arrays.stream(winner).map(Car::getName).toArray(String[]::new);
         System.out.println(String.join(", ", winnerNames) +  "가 최종 우승했습니다.");
+    }
+
+    private static void printGameProcess(GameControl gameControl, Car[] cars) {
+        for (Car car: cars) {
+            gameControl.carControl(car);
+            System.out.print(car.getName() + " : " + car.showDistance() + "\n");
+        }
+        System.out.println();
     }
 }
