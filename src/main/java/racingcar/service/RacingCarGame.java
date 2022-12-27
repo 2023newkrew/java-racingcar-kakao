980d129dc69f4a2b7416ff1d48a1f33b8b68fac3
eddie.kk <eddie.kk@kakaocorp.com>
2022-12-27 23:32:34 +0900

    feat: 남은 게임 횟수 검증 기능 구현

--- src/main/java/racingcar/service/RacingCarGame.java
+++ src/main/java/racingcar/service/RacingCarGame.java
@@ -4,6 +4,7 @@ import racingcar.domain.Car;
 import racingcar.dto.GameResult;
 import racingcar.dto.CarDto;
 import racingcar.utils.RacingCarConverter;
+import racingcar.utils.RacingCarValidator;
 import racingcar.utils.RandomNumberGenerator;
 
 import java.util.List;
@@ -14,14 +15,17 @@ public class RacingCarGame {
     private List<Car> cars;
     private int round;
     private RandomNumberGenerator randomNumberGenerator;
+    private RacingCarValidator racingCarValidator;
 
     public RacingCarGame(List<CarDto> carDtos, int gameRound) {
-        this.cars = RacingCarConverter.toCars(carDtos);
-        this.round = gameRound;
-        this.randomNumberGenerator = new RandomNumberGenerator();
+        cars = RacingCarConverter.toCars(carDtos);
+        round = gameRound;
+        randomNumberGenerator = new RandomNumberGenerator();
+        racingCarValidator = new RacingCarValidator();
     }
 
     public GameResult doNextRound() {
+        racingCarValidator.validateRemainingGameRound(round);
         for (Car car : cars) {
             car.move(randomNumberGenerator.generateBetweenZeroAndNine());
         }
--- src/main/java/racingcar/utils/RacingCarValidator.java
+++ src/main/java/racingcar/utils/RacingCarValidator.java
@@ -6,6 +6,7 @@ public class RacingCarValidator {
 
     private static final String CAR_NAME_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
     private static final String GAME_ROUND_ERROR_MESSAGE = "시도할 횟수는 0회 이상이어야 합니다.";
+    private static final String REMAINING_GAME_ROUND_ERROR_MESSAGE = "게임이 종료되었습니다.";
 
     public void validateCarNames(String[] carNames) {
         Arrays.stream(carNames)
@@ -17,6 +18,12 @@ public class RacingCarValidator {
     public void validateGameRound(int gameRound) {
         if (gameRound < 1) {
             throw new RuntimeException(GAME_ROUND_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateRemainingGameRound(int gameRound) {
+        if (gameRound <= 0) {
+            throw new RuntimeException(REMAINING_GAME_ROUND_ERROR_MESSAGE);
         }
     }
 
