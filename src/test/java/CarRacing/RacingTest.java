08fc23281f9b2282abe281ca31affbaa40bb4a29
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-27 11:55:19 +0900

    refac: 중복된 코드 setup으로 제거

--- src/test/java/CarRacing/RacingTest.java
+++ src/test/java/CarRacing/RacingTest.java
@@ -1,5 +1,6 @@
 package CarRacing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,19 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingTest {
 
-    //자동차 이름 스플릿 로직 작성
-    //자동차 이름 예외처리 로직 작성
     //자동차 객체 배열 생성
     //시도 회수 입력 받기
     //자동차들 각각에 전진 이벤트 부여 메소드 작성
     //시도 회수만큼 이벤트 발생하는 메소드 작성
     //출력 메소드 작성
     //우승자 판별 로직 작성
+    private Racing racing;
+
+    @BeforeEach
+    void setUp() {
+        racing = new Racing();
+    }
 
     @Test
     public void nameSplitTest() {
         String inputString = "pobi,crong,honux";
-        Racing racing = new Racing();
         assertThat(racing.nameSplit(inputString)).isEqualTo(new String[] {"pobi", "crong", "honux"});
     }
 
@@ -27,7 +31,6 @@ public class RacingTest {
     public void nameExceptionTest() {
         String wrong_name = "leopss";
         String correct_name = "pobi";
-        Racing racing = new Racing();
         assertThrows(RuntimeException.class, () -> {racing.nameException(wrong_name);});
         assertDoesNotThrow(() -> {racing.nameException(correct_name);});
     }
