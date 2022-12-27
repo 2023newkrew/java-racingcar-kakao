110f23cfba8bbd7f82056e315a69a1fc6b2d2b92
kane.woz <kane.woz@kakaocorp.com>
2022-12-27 11:12:15 +0900

    feat(racing): Car name에 대한 기능 구현

--- src/main/java/com/racing/Car.java
+++ src/main/java/com/racing/Car.java
@@ -1,10 +1,14 @@
 package com.racing;
 
 public class Car {
+    private String carName;
     public Car(String carName) {
-        
+        this.carName = carName;
     }
 
     public void checkName() {
+        if(carName.length() > 5){
+            throw new RuntimeException("car name length over 5");
+        }
     }
 }
