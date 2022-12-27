2c0dd5f8634d07e7a863f977e9377c4b8f4ea6a2
jgus52 <srraight@gmail.com>
2022-12-27 13:35:31 +0900

    Feat: Validator isValidTurn 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -11,4 +11,16 @@ public class Validator {
         }
         return true;
     }
+
+    public static boolean isValidTurn(String turn) {
+        int parsedTurn;
+
+        try {
+            parsedTurn = Integer.parseInt(turn);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return parsedTurn > 0;
+    }
 }
--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,5 +17,16 @@ public class ValidatorTest {
     @ValueSource(strings = {"asdasdasd", "azx1@!$", "afxvcx@3", "", " ", "ass "})
     void invalidName(String name) {
         assertThat(Validator.validateName(name)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "z"})
+    void invalidTurn(String turn) {
+        assertThat(Validator.isValidTurn(turn)).isFalse();
+    }
+
+    @Test
+    void validTurn() {
+        assertThat(Validator.isValidTurn("12")).isTrue();
     }
 }
