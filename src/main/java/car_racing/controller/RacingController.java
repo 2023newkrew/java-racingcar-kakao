c2b86032b27bd6c08bc7c98d951dc54130565cd9
BaeRoNuI <xogh20321@gmail.com>
2022-12-28 00:17:11 +0900

    refactor: apply custom exception

--- src/main/java/car_racing/controller/RacingController.java
+++ src/main/java/car_racing/controller/RacingController.java
@@ -1,39 +1,33 @@
 package car_racing.controller;
 
+import car_racing.common.exception.InvalidInputFormatException;
 import car_racing.model.Car;
 import car_racing.model.GameRule;
 import car_racing.model.RacingPlayer;
 import car_racing.view.InputView;
 import car_racing.view.OutputView;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingController {
-    private final InputView inputView;
-    private final List<RacingPlayer> racingPlayers;
-    private String[] playerNames;
+    private final InputView inputView = new InputView();
+    private List<RacingPlayer> racingPlayers;
     private Integer numberOfTurns;
 
-
-    public RacingController() {
-        racingPlayers = new ArrayList<>();
-        inputView = new InputView();
-    }
     public void run() {
         try {
-            getNamesFromUser();
+            createPlayers(getNamesFromUser());
             getNumberOfTurnsFromUser();
-        } catch (Exception e) {
+        } catch (InvalidInputFormatException e) {
             System.out.println(e.getLocalizedMessage());
             return;
         }
 
-        createPlayers();
-        printResult();
+        runWholeGame();
     }
-    private void getNamesFromUser() {
+    private List<String> getNamesFromUser() {
         OutputView.askForNames();
-        playerNames = inputView.getPlayerNames();
+        return inputView.getPlayerNames();
     }
 
     private void getNumberOfTurnsFromUser() {
@@ -41,36 +35,32 @@ public class RacingController {
         numberOfTurns = inputView.getNumberOfTurns();
     }
 
-    private void printResult() {
+    private void runWholeGame() {
         System.out.println();
         System.out.println("실행결과");
         OutputView.printCurrentStatus(racingPlayers);
         for (int i = 0; i < numberOfTurns; i++) {
-            sleep();
+            sleepForMillis(500);
             runSingleTurn();
             OutputView.printCurrentStatus(racingPlayers);
         }
         OutputView.printWinners(GameRule.getWinners(racingPlayers));
     }
 
-    private void createPlayers() {
-        for (String playerName :
-                playerNames) {
-            racingPlayers.add(new Car(playerName));
-        }
+    private void createPlayers(List<String> playerNames) {
+        racingPlayers = playerNames.stream()
+            .map(Car::new)
+            .collect(Collectors.toList());
     }
 
     private void runSingleTurn() {
-        for (RacingPlayer player :
-                racingPlayers) {
-            boolean proceed = GameRule.isAbleToProceed();
-            player.proceedNextTurn(proceed);
-        }
+        racingPlayers
+            .forEach(racingPlayer -> racingPlayer.proceedNextTurn(GameRule.isAbleToProceed()));
     }
     
-    private void sleep() {
+    private void sleepForMillis(Integer milliSeconds) {
         try {
-            Thread.sleep(500);
+            Thread.sleep(milliSeconds);
         } catch(InterruptedException e) {
             System.out.println(e.getLocalizedMessage());
         }
