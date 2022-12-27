cf9954c7fe2a2adf01665c5e0f726fa168babfbb
kyne.k <dd2307@snu.ac.kr>
2022-12-27 17:54:34 +0900

    feat(racing): 기능 6,7,8 구현
    - InputValidator 생성 및 구현
    - inputCarNames를 검증하고 List<String>을 반환
      - validateCarNames(), validateSingleCarName() 메서드
    - inputRepeat를 검증하고 int를 반환
      - validateGameRepeat(), validatePositive() 메서드

---
+++ src/main/java/racing/utils/InputValidator.java
@@ -0,0 +1,51 @@
+package racing.utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputValidator {
+    public static final String DELIMITER = ",";
+    public static final String CAR_NAME_DUPLICATE_EXCEPTION_MESSAGE = "[ERROR] 이름이 중복되었습니다.";
+    public static final String CAR_NAME_LENGTH_EXCEPTION_MESSAGE = "[ERROR] 이름은 5자 이하여야 합니다.";
+    public static final String INPUT_REPEAT_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 반복 횟수는 정수여야 합니다.";
+    public static final String INPUT_REPEAT_RANGE_EXCEPTION_MESSAGE = "[ERROR] 반복 횟수는 양수여야 합니다.";
+    public static final int CAR_NAME_MAX_LENGTH = 5;
+    private Set<String> carNameSet;
+
+    public List<String> validateCarNames(String inputCarNames) {
+        carNameSet = new HashSet<>();
+        List<String> carNames = Arrays.asList(inputCarNames.split(DELIMITER));
+        carNames.forEach(this::validateSingleCarName);
+        return carNames;
+    }
+
+    private void validateSingleCarName(String carName) {
+        if (carNameSet.contains(carName)) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATE_EXCEPTION_MESSAGE);
+        }
+        carNameSet.add(carName);
+        if (carName.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public int validateGameRepeat(String inputRepeat) {
+        int repeat;
+        try {
+            repeat = Integer.parseInt(inputRepeat);
+            validatePositive(repeat);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_REPEAT_FORMAT_EXCEPTION_MESSAGE);
+        }
+        return repeat;
+    }
+
+    private void validatePositive(int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(INPUT_REPEAT_RANGE_EXCEPTION_MESSAGE);
+        }
+    }
+
+}
