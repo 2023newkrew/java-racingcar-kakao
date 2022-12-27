a5b12fe286bfc9690a80ea731f3b36a6fcbe97c3
Minsu Kim <ravor8878@gmail.com>
2022-12-27 15:18:25 +0900

    fix : add Car.equals

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -39,6 +39,18 @@ public class Car implements Comparable{
         return this.position.compareTo(((Car) o).position);
     }
 
+    @Override
+    public boolean equals(Object o){
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(position, car.position);
+    }
+
     public boolean equalsPosition(Object o) {
         if (this == o){
             return true;
