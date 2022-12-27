d03039fe41fe08ab9969a93cf753b92c82b39bbc
pyun0825 <pyun0825@naver.com>
2022-12-27 11:36:28 +0900

    feat: add class RandomPlayStrategy

---
+++ src/main/java/car_racing/RandomPlayStrategy.java
@@ -0,0 +1,9 @@
+package car_racing;
+
+public class RandomPlayStrategy implements GamePlayStrategy {
+
+    @Override
+    public Integer generateScore() {
+        return (int) (Math.random() * 9);
+    }
+}
