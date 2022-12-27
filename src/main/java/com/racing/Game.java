6d30a5c030a27e0a5e9799d9c2f4ed18f23b8b60
kane.woz <kane.woz@kakaocorp.com>
2022-12-27 11:43:28 +0900

    test(racing): Game init 테스트코드 작성

---
+++ src/main/java/com/racing/Game.java
@@ -0,0 +1,10 @@
+package com.racing;
+
+import java.util.ArrayList;
+
+public class Game {
+    public ArrayList<Car> cars;
+
+    public void init(String s) {
+    }
+}
---
+++ src/test/java/com/racing/GameTest.java
@@ -0,0 +1,21 @@
+package com.racing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class GameTest {
+
+    @Test
+    void initTest(){
+        Game game = new Game();
+        game.init("car1,car2,car3");
+        // init에서 3개의 자동차를 선언했으므로 길이가 3이여야 함
+        assertThat(game.cars).hasSize(3);
+        // 각 자동차는 이동거리가 0인 자동차이고 각각의 이름은 car1, car2, car3 이므로 아래와 같은 출력값을 가져야 한다.
+        assertThat(game.cars.get(0).printLocation()).isEqualTo("car1 : ");
+        assertThat(game.cars.get(1).printLocation()).isEqualTo("car2 : ");
+        assertThat(game.cars.get(2).printLocation()).isEqualTo("car3 : ");
+    }
+}
