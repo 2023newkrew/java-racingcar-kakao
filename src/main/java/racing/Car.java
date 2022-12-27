4963c94bc1ce2fcb7ebb39df4a3cafd15935fca5
Neurumaru <ksj6659@gmail.com>
2022-12-27 15:04:32 +0900

    feat(Car): 자동차 move 추가
    자동차가 움직여 위치가 이동하는 함수 구현

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -2,7 +2,7 @@ package racing;
 
 public class Car {
     private final String name;
-    private int position = 0;
+    private int position = 1;
 
     public Car(String name) {
         if (name.isEmpty() || name.length() > 5) {
@@ -11,9 +11,13 @@ public class Car {
         this.name = name;
     }
 
+    public void move() {
+        this.position += 1;
+    }
+
     @Override
     public String toString() {
-        String positionString = "-".repeat(position + 1);
+        String positionString = "-".repeat(position);
         return String.format("%s : %s", name, positionString);
     }
 }
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,7 +1,10 @@
 package racing;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -25,5 +28,23 @@ public class CarTest {
     void toString(String name) {
         Car car = new Car(name);
         assertThat(car.toString()).isEqualTo(String.format("%s : -", name));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getMoveData")
+    void move(String name, int move, String expected) {
+        Car car = new Car(name);
+        for (int i = 0; i < move; i++) {
+            car.move();
+        }
+        assertThat(car.toString()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> getMoveData() {
+        return Stream.of(
+                Arguments.of("ABCD", 2, "ABCD : ---"),
+                Arguments.of("ABCD", 3, "ABCD : ----"),
+                Arguments.of("ABCD", 4, "ABCD : -----")
+        );
     }
 }
