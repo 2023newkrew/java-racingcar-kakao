f2642f36e0523ae8df912fecaaf168a7315049be
kane.woz <kane.woz@kakaocorp.com>
2022-12-27 11:48:21 +0900

    feat(racing): Game init 구현

--- src/main/java/com/racing/Game.java
+++ src/main/java/com/racing/Game.java
@@ -5,6 +5,16 @@ import java.util.ArrayList;
 public class Game {
     public ArrayList<Car> cars;
 
-    public void init(String s) {
+    public Game(){
+        this.cars = new ArrayList<>();
+    }
+
+    public void init(String text) {
+        // text(사용자 입력)을 차 이름으로 분리
+        String[] carNames = text.split(",");
+
+        for(String carName : carNames){
+            cars.add(new Car(carName));
+        }
     }
 }
