5d993dcb249bf56cd737b0b78114c623848fb1a5
Nelfunction <oat641@gmail.com>
2022-12-27 22:38:07 +0900

    TDD - 자동차 경주 코드리뷰 요청 (#6)
    * docs: add requirements
    
    * feat: save changes
    
    * test: complete perfect test code
    
    * feat: complete perfect production code
    
    * feat: separate package
    
    * feat: racing car init
    
    * test: generate cars
    
    * feat: generate cars
    
    * test: car control
    
    * feat: car control
    
    * feat: compare car objects
    
    * test: get winner
    
    * feat: get winner
    
    * feat: main application
    
    * fix: car control
    
    * fix: extract methods
    
    * test: empty input

---
+++ src/main/java/string_calculator/model/Parser.java
@@ -0,0 +1,57 @@
+package string_calculator.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    private final List<String> delimiters = new ArrayList<>(Arrays.asList(",", ":"));
+
+    private final String equation;
+
+    public Parser(String inputString) {
+        inputString = emptyNull(inputString);
+        String customDelimiter = extractCustomDelimiter(inputString);
+        Optional.ofNullable(customDelimiter).map(delimiters::add);
+        this.equation = inputString;
+    }
+
+    public String[] parse() {
+        return separateByDelimiter(extractPureEquation(equation));
+    }
+
+    private String emptyNull(String inputString) {
+        if (inputString == null) {
+            return "0";
+        }
+        if (inputString.length() == 0) {
+            return "0";
+        }
+        return inputString;
+    }
+
+    private String[] separateByDelimiter(String equation) {
+        String regex = String.join("|", delimiters);
+        return equation.split(regex);
+    }
+
+    private String extractCustomDelimiter(String inputString) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputString);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return null;
+    }
+
+    private String extractPureEquation(String inputString) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputString);
+        if (m.find()) {
+            return m.group(2);
+        }
+        return inputString;
+    }
+}
