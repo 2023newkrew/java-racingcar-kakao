f73817722d8cbf912239b5df2e01fa669bd89e4a
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-27 11:11:02 +0900

    test: 자동차 전진 여부 테스트 코드 작성

--- src/test/java/CarRacing/CarTest.java
+++ src/test/java/CarRacing/CarTest.java
@@ -1,5 +1,6 @@
 package CarRacing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,9 +8,21 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("test");
+    }
+
+    @Test
+    public void isMoveTest() {
+        assertThat(car.isMove(3)).isEqualTo(false);
+        assertThat(car.isMove(4)).isEqualTo(true);
+    }
     @Test
     public void moveCarTest() {
-        Car car = new Car();
         car.moveCar(true);
         assertThat(car.getPosition()).isEqualTo(2);
 
