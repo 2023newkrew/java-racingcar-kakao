913cf3c7d0d271faeaa06032a0102b87667ed260
queueSub <oscar.award@kakaocorp.com>
2022-12-27 15:44:59 +0900

    feat: 한 턴을 플레이하는 기능

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class RacingGame {
     private List<RacingCar> cars;
+    private int finalTurn;
+    private int turnCount;
+
+    public int getTurnCount() {
+        return this.turnCount;
+    }
 
     public RacingGame() {
         this.cars = new ArrayList<>();
     }
-    public RacingGame(List<String> names) {
+    public RacingGame(List<String> names, int finalTurn) {
         this();
         for (String name : names) {
             cars.add(new RacingCar(name));
@@ -18,5 +24,12 @@ public class RacingGame {
 
     public int generateRandomNumber() {
         return (int)(Math.random()*1000) %10;
+    }
+
+    public void proceedTurn() {
+        for(RacingCar rc : cars) {
+            rc.accelerate(this.generateRandomNumber());
+        }
+        this.turnCount++;
     }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -7,4 +7,18 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class RacingGameTest {
+
+    @Test
+    @DisplayName("한 턴을 플레이하는 기능")
+    public void playTurn() {
+        //given
+        RacingGame racingGame = new RacingGame(List.of("abc","efg","hijkl"),5);
+
+        //when
+        racingGame.proceedTurn();
+        racingGame.proceedTurn();
+
+        //then
+        assertEquals(2,racingGame.getTurnCount());
+    }
 }
