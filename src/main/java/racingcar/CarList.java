908fc80969fd72c4296489aa54d09bb52c65fe66
jungin0507 <a01033046758@gmail.com>
2022-12-27 16:44:36 +0900

    [feat] 우승자 선택 로직 추가

--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
 
 public class CarList {
     private final List<Car> cars;
--- src/test/java/racingcar/CarListTest.java
+++ src/test/java/racingcar/CarListTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class CarListTest {
 
     @Test
@@ -13,5 +15,21 @@ public class CarListTest {
                     carList.add(new Car("lion"));
                     carList.add(new Car("lion"));
                 });
+    }
+
+    @Test
+    void selectWinners() {
+        CarList carList = new CarList();
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+        car1.move(5);
+        car2.move(5);
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        List<Car> winner = carList.selectWinners();
+        Assertions.assertThat(winner)
+                .containsOnly(car1, car2);
     }
 }
