f4e80474f1fb6b326731dbdbb6db632c195b49a7
yooj929 <jang0929@naver.com>
2022-12-27 10:56:25 +0900

    feat: inputName 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String[] inputName() {
+        String[] names = scanner.nextLine().split(" ,");
+        for (String name : names) {
+            validate(name);
+        }
+        return names;
+    }
+
+    private void validate(String name) {
+        if(name.length() >5)
+            throw new RuntimeException();
+    }
+}
