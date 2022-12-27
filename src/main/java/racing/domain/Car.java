82cc8c2f6da2acc0a3760bed1609925113bb1c4e
kyne.k <dd2307@snu.ac.kr>
2022-12-27 13:49:49 +0900

    feat(racing): 기능 4 구현
    - Car 클래스가 Comparable을 구현하도록 수정
    - compareTo() 메서드를 distance를 비교하도록 override
    - getDifference() 메서드 구현

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,9 +1,9 @@
 package racing.domain;
 
-public class Car {
+public class Car implements Comparable{
     private final double MOVE_LOWER_BOUND = 4.0;
     private int distance;
-    private final String name;
+    public final String name;
 
     public Car(String name) {
         this.name = name;
@@ -24,5 +24,15 @@ public class Car {
 
     private boolean isMoving(double number) {
         return number >= MOVE_LOWER_BOUND;
+    }
+
+    public int getDifference(int opponentDistance) {
+        return opponentDistance - distance;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Car car = (Car) o;
+        return car.getDifference(distance);
     }
 }
