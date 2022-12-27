3f19460e7f49b8b08f7823d5ee04ef2b2c831e7e
kane.woz <kane.woz@kakaocorp.com>
2022-12-27 15:40:18 +0900

    doc(racing): Game, Car, Main의 주석을 추가함.

--- src/main/java/com/racing/Car.java
+++ src/main/java/com/racing/Car.java
@@ -7,16 +7,27 @@ public class Car {
         this.carName = carName;
     }
 
+    /**
+     * 차 이름이 5글자 초과라면 RuntimeError
+     */
     public void checkName() {
         if(carName.length() > 5){
             throw new RuntimeException("car name length over 5");
         }
     }
 
+    /**
+     * random number가 4 이상이라면 한 칸 전진
+     * @param rand
+     */
     public void movement(int rand) {
         if(rand >= 4) this.location++;
     }
 
+    /**
+     * 자동차의 현재 위치를 형식에 맞게 출력
+     * @return 자동차의 location 출력
+     */
     public String printLocation() {
         return String.format(
                 "%s : %s",
