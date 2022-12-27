5b8683c73b05547435479afeab6261d35bda729f
HONGMINJU <mhong0800@gmail.com>
2022-12-27 18:16:42 +0900

    feat: 입력 메세지 출력 기능 추가

--- src/main/java/racingcar/RacingGameInfoReader.java
+++ src/main/java/racingcar/RacingGameInfoReader.java
@@ -14,6 +14,7 @@ public class RacingGameInfoReader {
     }
 
     public List<RacingCar> readRacingCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scanner.next();
         String[] racingCarNames = input.split(",");
         validateRacingCarNames(racingCarNames);
@@ -39,6 +40,7 @@ public class RacingGameInfoReader {
     }
 
     public int readRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
         return scanner.nextInt();
     }
 }
