63e72c66cf29e3fec4175f9ddc70b19a825bc450
jgus52 <srraight@gmail.com>
2022-12-27 11:17:27 +0900

    Refactor: 패키지 소문자 변경

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,5 @@
-import Calculator.Calculator;
+package calculator;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
