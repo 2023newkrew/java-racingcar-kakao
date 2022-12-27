a8d8fbb483a32e4d04890705d7c365c88880511a
Nelfunction <oat641@gmail.com>
2022-12-27 14:41:23 +0900

    feat: get winner

--- src/main/java/racing_car/model/GameControl.java
+++ src/main/java/racing_car/model/GameControl.java
@@ -1,8 +1,13 @@
 package racing_car.model;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class GameControl {
+
     public int generateRandomNumber() {
         return new Random().nextInt(10);
     }
@@ -11,5 +16,13 @@ public class GameControl {
         if (number >= 4){
             car.move(1);
         }
+    }
+
+    public Car[] getWinners(Car[] cars) {
+        Car maxDistance = Arrays.stream(cars).max(Comparator.naturalOrder()).get();
+        return Arrays.stream(cars)
+                .filter(car -> car.compareTo(maxDistance) == 0)
+                .collect(Collectors.toList())
+                .toArray(Car[]::new);
     }
 }
