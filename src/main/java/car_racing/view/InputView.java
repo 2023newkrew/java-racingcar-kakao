2c4610480c9c526856ee6de47f32b8b82115c73f
BaeRoNuI <xogh20321@gmail.com>
2022-12-28 00:17:51 +0900

    refactor: remove meaningless conversion overhead

--- src/main/java/car_racing/view/InputView.java
+++ src/main/java/car_racing/view/InputView.java
@@ -1,27 +1,27 @@
 package car_racing.view;
 
 import car_racing.common.exception.InvalidInputFormatException;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     Scanner sc = new Scanner(System.in);
 
-    public String[] getPlayerNames() {
+    public List<String> getPlayerNames() {
         String inputString = sc.nextLine();
-        String[] splitStrings = inputString.split(",", -1);
-        for (String name :
-                splitStrings) {
-            verifyName(name);
-        }
+        List<String> splitStrings = List.of(inputString.split(",", -1));
+        splitStrings.forEach(this::verifyName);
 
         return splitStrings;
     }
 
     public Integer getNumberOfTurns() {
         int turn = sc.nextInt();
+
         if (turn <= 0) {
             throw new InvalidInputFormatException("Invalid number for turns");
         }
+
         return turn;
     }
 
