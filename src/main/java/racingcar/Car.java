5c124703e250d14a87482e3288dca23a6d00ae36
sean.m <sean.m@kakaocorp.com>
2022-12-27 13:08:47 +0900

    feat(레이싱카): 자동차 전진시도 기능 구현
    자동차 전진시도 메소드 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void forward() {
+        distance += (int)(Math.random() * 10) >= 4 ? 1 : 0;
+    }
+
+    public int getDistance(){
+        return this.distance;
+    }
+
+
+}
