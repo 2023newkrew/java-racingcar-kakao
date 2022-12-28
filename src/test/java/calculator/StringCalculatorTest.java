d1a16e7855a22e9df7ed75a4f21e7ccc2b7db530
sean.m <sean.m@kakaocorp.com>
2022-12-28 20:05:09 +0900

    test(calculator): 계산기 테스트코드 개선
    계산기 테스트코드 개선
     - 계산기 테스트코드 한글화
     - 테스트케이스 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,57 +1,58 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
     @Test
-    void emptyString() {
+    void 빈_문자열_입력시_0반환() {
         StringCalculator emptyStr = new StringCalculator("");
         assertThat(emptyStr.calculate()).isEqualTo(0);
     }
 
     @Test
-    void nullString() {
+    void 널_문자열_입력시_0반환() {
         StringCalculator nullStr = new StringCalculator(null);
         assertThat(nullStr.calculate()).isEqualTo(0);
     }
 
-    @Test
-    void basicString() {
-        StringCalculator basic = new StringCalculator("1,2:3");
-        assertThat(basic.calculate()).isEqualTo(6);
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3|6", "22,33,11:1:3|70"}, delimiter = '|')
+    void 기본적인_문자열_입력_테스트(String str, int answer) {
+        StringCalculator basic = new StringCalculator(str);
+        assertThat(basic.calculate()).isEqualTo(answer);
     }
 
     @Test
-    void numberString() {
+    void 숫자만_있는_문자열_입력_테스트() {
         StringCalculator number = new StringCalculator("123123");
         assertThat(number.calculate()).isEqualTo(123123);
     }
 
     @Test
-    void invalidString() {
+    void 사용_불가능한_문자열_입력_테스트() {
         StringCalculator invalid = new StringCalculator("1[2,3:4");
         assertThatThrownBy(invalid::calculate).isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    void invalidCustomString() {
+    void 사용_불가능한_커스텀_문자_입력() {
         StringCalculator invalid = new StringCalculator("//$\n1@2,3:4");
         assertThatThrownBy(invalid::calculate).isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    void customString() {
+    void 커스텀_문자_정상_사용() {
         StringCalculator custom = new StringCalculator("//[!$#&\n1,2:3[4");
         assertThat(custom.calculate()).isEqualTo(10);
     }
 
     @Test
-    void minusString() {
-        assertThatThrownBy(() -> {
-            new StringCalculator("-12,3:4");
-        }).isInstanceOf(RuntimeException.class);
+    void 음수_문자열_입력() {
+        assertThatThrownBy(() -> new StringCalculator("-12,3:4")).isInstanceOf(RuntimeException.class);
     }
 }
