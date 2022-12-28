6cf769f35d0279e886ef08d96080a4dd9d6feb77
sean.m <sean.m@kakaocorp.com>
2022-12-28 15:00:48 +0900

    test: 전진 시도 유닛테스트 수정 개선
    전진 시도 유닛테스트 수정 개선
     - 무조건 전진하거나 정지하는 전략을 람다로 전달하여 무작위 제거 후 테스트하도록 수정

--- src/test/java/racingcar/service/CarTest.java
+++ src/test/java/racingcar/service/CarTest.java
@@ -6,9 +6,16 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
     @Test
-    void carForward() {
-        Car car = new Car("davi");
+    void 자동차가_앞으로_전진한다() {
+        Car car = new Car("davi", () -> true);
         car.forward();
-        assertThat(car.getDistance()).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(1);
+        assertThat(car.getDistance()).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차가_정지한다() {
+        Car car = new Car("sean", () -> false);
+        car.forward();
+        assertThat(car.getDistance()).isEqualTo(0);
     }
 }
