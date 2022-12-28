f588049079d1fe27561d6e49ef8057edc4984e25
kyne.k <dd2307@snu.ac.kr>
2022-12-28 20:08:15 +0900

    refactor(racing): RandomNumberGenerator 재생성
    - RandomlyMovable 클래스에서 사용함을 확인하여 재생성

---
+++ src/main/java/racing/utils/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package racing.utils;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    private static final int LOWER_BOUND = 0;
+    private static final int UPPER_BOUND = 9;
+    private static final Random random = new Random();
+
+    public static int generate() {
+        return LOWER_BOUND + random.nextInt(UPPER_BOUND - LOWER_BOUND + 1);
+    }
+}
