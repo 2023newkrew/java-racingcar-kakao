e83c6ca3f3e8cb286770a16002825df5fd21b9b0
ekzm8523 <mjw8523@gmail.com>
2022-12-28 18:56:09 +0900

    refactor: Car 생성자 체이닝 방식으로 변경

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -5,8 +5,7 @@ public class Car {
     private int position;
     private static final int DEFAULT_POSITION = 0;
     public Car(String name) {
-        this.name = new Name(name);
-        this.position = DEFAULT_POSITION;
+        this(name, DEFAULT_POSITION);
     }
 
     public Car(String name, int position) {
