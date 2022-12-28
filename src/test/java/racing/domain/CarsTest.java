d00eab91f903024f980a0fc3aff08feeef60c533
ekzm8523 <mjw8523@gmail.com>
2022-12-28 18:37:30 +0900

    test: Cars domain 테스트 코드 수정

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -1,7 +1,8 @@
 package racing.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,16 +15,16 @@ public class CarsTest {
         cars = new Cars();
     }
 
-    @Test
-    public void isMove() {
-        assertThat(cars.isMove(4)).isEqualTo(true);
-        assertThat(cars.isMove(9)).isEqualTo(true);
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    public void 자동차는_4_이상의_숫자에서_움직입니다(int number) {
+        assertThat(cars.isMove(number)).isEqualTo(true);
     }
 
-    @Test
-    public void isStop() {
-        assertThat(cars.isMove(0)).isEqualTo(false);
-        assertThat(cars.isMove(3)).isEqualTo(false);
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    public void 자동차는_3_이하의_숫자에서는_움직이지_않습니다(int number) {
+        assertThat(cars.isMove(number)).isEqualTo(false);
     }
 
 }
