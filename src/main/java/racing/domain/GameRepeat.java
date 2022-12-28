f596a8e5cbac5152aed2c1113fd5a5d404498f83
kyne.k <dd2307@snu.ac.kr>
2022-12-28 20:08:15 +0900

    feat(racing): GameRepeat 클래스 생성 및 구현
    - 반복 횟수에 대한 검증 진행
    - isRemaining(), reduce() 메서드 구현

---
+++ src/main/java/racing/domain/GameRepeat.java
@@ -0,0 +1,31 @@
+package racing.domain;
+
+public class GameRepeat {
+    public static final int MIN_REPEAT = 1;
+    public static final int MAX_REPEAT = 100;
+    public static final String NOT_ENOUGH_REPEAT_EXCEPTION_MESSAGE = "[ERROR] 최소 1회는 시도해야 합니다.";
+    public static final String TOO_MANY_REPEAT_EXCEPTION_MESSAGE = "[ERROR] 시도 횟수는 최대 100회까지만 가능합니다.";
+
+    private int remaining;
+    public GameRepeat(int repeat) {
+        validate(repeat);
+        this.remaining = repeat;
+    }
+
+    public void validate(int repeat) {
+        if (repeat < MIN_REPEAT) {
+            throw new IllegalArgumentException(NOT_ENOUGH_REPEAT_EXCEPTION_MESSAGE);
+        }
+        if (repeat > MAX_REPEAT) {
+            throw new IllegalArgumentException(TOO_MANY_REPEAT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public void reduce() {
+        remaining--;
+    }
+
+    public boolean hasRemaining() {
+        return remaining > 0;
+    }
+}
