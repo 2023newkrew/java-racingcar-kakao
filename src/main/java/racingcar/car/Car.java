e2824bcf8e644cb8e6818672e13ce70c5ceaba84
jgus52 <srraight@gmail.com>
2022-12-28 14:04:05 +0900

    Refactor: Car name 검증 로직 Car 생성자로 이동

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,11 +1,23 @@
 package racingcar.car;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 0;
+
     private int position = 0;
     private final String name;
 
     public Car(String name) {
+        if (!isValidSingleName(name)) throw new IllegalArgumentException("Invalid Name");
+
         this.name = name;
+    }
+
+    public static boolean isValidSingleName(String name) {
+        if (name.length() > MAX_NAME_LENGTH || name.length() <= MIN_NAME_LENGTH) {
+            return false;
+        }
+        return !name.contains(" ");
     }
 
     public void move(CarAction carAction) {
--- src/main/java/racingcar/racing/Validator.java
+++ src/main/java/racingcar/racing/Validator.java
@@ -1,23 +1,14 @@
 package racingcar.racing;
 
+import racingcar.car.Car;
+
 import java.util.List;
 
 public class Validator {
-    private static final int MAX_NAME_LENGTH = 5;
-    private static final int MIN_NAME_LENGTH = 0;
-
-
-    public static boolean isValidSingleName(String name) {
-        if (name.length() > MAX_NAME_LENGTH || name.length() <= MIN_NAME_LENGTH) {
-            return false;
-        }
-        return !name.contains(" ");
-    }
-
     public static boolean isValidNames(List<String> names) {
         if (names.size() != names.stream().distinct().count()) return false;
 
-        return names.stream().allMatch(Validator::isValidSingleName);
+        return names.stream().allMatch(Car::isValidSingleName);
     }
 
     public static boolean isValidTurn(String turn) {
