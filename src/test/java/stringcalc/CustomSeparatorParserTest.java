8e7ec002b32dfbd4df0d95b52aac1276179c7163
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-28 14:44:06 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/stringcalc/CustomSeparatorParserTest.java
+++ src/test/java/stringcalc/CustomSeparatorParserTest.java
@@ -1,12 +1,9 @@
 package stringcalc;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
