8e01da3bda18a8ab98e1804152b9aa0b33714754
kyne.k <dd2307@snu.ac.kr>
2022-12-28 18:05:25 +0900

    refactor(racing): Car 클래스의 Movable 필드 추가, move() 구현, 비교 메서드 제거
    - Comparable 구현 제거
    - Movable 필드 및 생성자 추가
    - 매개변수 없는 move() 구현

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,16 +1,30 @@
 package racing.domain;
 
 import racing.dto.CarDTO;
+import racing.movable.Movable;
+import racing.movable.RandomlyMovable;
 
-public class Car implements Comparable<Object> {
+public class Car {
     private static final int MOVING_LOWER_BOUND = 4;
     private static final String MOVING_SYMBOL = "-";
     private static final String STATUS_STRING_FORMAT = "%s : %s";
     private final String name;
+    private final Movable movable;
     private int distance = 0;
 
     public Car(final String name) {
+        this(name, new RandomlyMovable());
+    }
+
+    public Car(final String name, final Movable movable){
         this.name = name;
+        this.movable = movable;
+    }
+
+    public void move() {
+        if (movable.isMoving()) {
+            increaseDistance();
+        }
     }
 
     public boolean move(final int number) {
@@ -27,16 +41,6 @@ public class Car implements Comparable<Object> {
 
     private boolean isMoving(int number) {
         return number >= MOVING_LOWER_BOUND;
-    }
-
-    private int getDifference(int opponentDistance) {
-        return opponentDistance - distance;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        Car car = (Car) o;
-        return car.getDifference(distance);
     }
 
     @Override
