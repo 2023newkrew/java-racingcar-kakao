5cefe1d897c39e01989d78db81fe8880492aa59f
JongWoo Jeong <knight7024@naver.com>
2022-12-28 19:12:25 +0900

    test: 테스트코드 변경 - 불분명한 테스트 제거 - 새로운 테스트 추가

--- src/test/java/racing_car/RacingCarTest.java
+++ src/test/java/racing_car/RacingCarTest.java
@@ -2,53 +2,58 @@ package racing_car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racing_car.model.Car;
+import racing_car.model.Cars;
 import racing_car.model.Game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RacingCarTest {
 
-    private static Car[] separateCarNamesByDelimiter(String delimiter, String target) {
+    private static Cars separateCarNamesByDelimiter(String target) {
 
-        return Car.from(target.split(delimiter));
+        return Cars.of(target.split(","));
     }
 
     @Test
     @DisplayName("제공된 문자열로 자동차 배열 생성")
     void separateByDelimiter() {
         String testNames = "pobi,crong,honux";
-        Car[] cars = separateCarNamesByDelimiter(",", testNames);
-        assertArrayEquals(cars, new Car[]{
+        Cars cars = separateCarNamesByDelimiter(testNames);
+        assertEquals(cars, Cars.of(new Car[]{
                 new Car("pobi"),
                 new Car("crong"),
                 new Car("honux")
-        });
+        }));
     }
 
     @Test
-    @DisplayName("빈 문자열 테스트")
-    void emptyName() {
-        String testNames = "";
-        Car[] cars = Car.from(testNames.split(","));
-        assertArrayEquals(cars, new Car[]{
-                new Car("")
-        });
+    @DisplayName("이름이 빈 문자열일 경우 테스트")
+    void checkEmptyName() {
+        String testName = "";
+        Throwable t = assertThrowsExactly(IllegalArgumentException.class, () -> separateCarNamesByDelimiter(testName));
+        assertEquals(t.getMessage(), "이름은 1자 이상이여야 합니다.");
     }
 
     @Test
+    @DisplayName("이름이 5자 초과일 경우 테스트")
+    void checkNameMaxLength() {
+        String testName = "chunsik";
+        Throwable t = assertThrowsExactly(IllegalArgumentException.class, () -> separateCarNamesByDelimiter(testName));
+        assertEquals(t.getMessage(), "이름은 5자 이하여야 합니다.");
+    }
+
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @ParameterizedTest
     @DisplayName("차량 전진 제어")
-    void carControl() {
-        Game game = new Game(new Car[]{
-                new Car("pobi")
-        });
-        game.moveCar(0);
+    void carControl(int step) {
+        Car car = new Car("pobi");
+        car.move(step);
 
-        Car[] cars = game.getCars();
-        Car firstCar = cars[0];
-
-        String output = firstCar.showDistance();
-        assertTrue(output.equals("-") || output.equals(""));
+        String output = car.displayDistance();
+        assertEquals("-".repeat(step), output);
     }
 
     @Test
@@ -66,19 +71,19 @@ public class RacingCarTest {
     @Test
     @DisplayName("우승자 구하기")
     void getWinners() {
-        String testNames = "pobi,crong,honux,ryan,chunsik,jordy";
-        Car[] cars = separateCarNamesByDelimiter(",", testNames);
+        String[] testNames = {"pobi", "crong", "honux", "ryan", "chun", "jordy"};
+        Car[] cars = new Car[testNames.length];
 
         int[] distances = new int[]{1, 5, 2, 5, 4, 5};
         for (int i = 0; i < distances.length; i++) {
+            cars[i] = new Car(testNames[i]);
             cars[i].move(distances[i]);
         }
 
-        Game game = new Game(cars);
-        Car[] winners = game.getWinners();
+        Cars cars1 = Cars.of(cars);
 
-        assertArrayEquals(new Car[]{
+        assertEquals(Cars.of(new Car[]{
                 cars[1], cars[3], cars[5]
-        }, winners);
+        }), cars1.getWinners());
     }
 }
