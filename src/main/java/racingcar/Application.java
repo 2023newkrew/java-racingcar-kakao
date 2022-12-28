7c7e300e811c8874a5c7baeb3638f7aaf82a964e
wilgur513 <wilgur513@naver.com>
2022-12-28 15:34:46 +0900

    refactor: RacingSimulator 추출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,7 @@
 package racingcar;
 
-import java.util.List;
 import java.util.Random;
-import racingcar.model.Positions;
-import racingcar.model.RacingGame;
-import racingcar.model.Winners;
+import racingcar.model.NumberGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -13,21 +10,9 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView(System.out, System.in);
         OutputView outputView = new OutputView(System.out);
+        NumberGenerator randomNumberGenerator = () -> new Random().nextInt(9);
 
-        List<String> carNames = inputView.inputCarNames();
-        int maxTryCount = inputView.inputMaxTryCount();
-
-        RacingGame racingGame = new RacingGame(maxTryCount, ()->new Random().nextInt(9), carNames);
-
-        outputView.printExecuteResultMessage();
-
-        while(!racingGame.isFinished()){
-            racingGame.move();
-            Positions positions = racingGame.getPositions();
-            outputView.printPositions(positions);
-        }
-
-        Winners winners = racingGame.getWinners();
-        outputView.printWinners(winners);
+        RacingSimulator racingSimulator = new RacingSimulator(inputView, outputView);
+        racingSimulator.simulate(randomNumberGenerator);
     }
 }
