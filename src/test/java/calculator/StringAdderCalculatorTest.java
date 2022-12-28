55f5d7123b7f1567291305f348245081965dbced
엄태호 <qqsscc7342@gmail.com>
2022-12-28 13:59:29 +0900

    test: null값 적용을 위한 어노테이션 변경

--- src/test/java/calculator/StringAdderCalculatorTest.java
+++ src/test/java/calculator/StringAdderCalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
@@ -20,7 +21,7 @@ public class StringAdderCalculatorTest {
 
     @ParameterizedTest
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    @ValueSource(strings = {"", "  "})
+    @NullAndEmptySource
     public void empty(String input) {
         assertThat(calculator.calculate(new Prompt(input))).isEqualTo(0);
     }
