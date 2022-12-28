959547bde20112e6b824ccd76735ce8370b7cdc7
jeothen <jeothen@gmail.com>
2022-12-28 15:39:43 +0900

    feat: Random값에 따른 자동차 전진 여부 판단

---
+++ src/test/java/racingcar/MovableStrategyTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+public class MovableStrategyTest {
+
+    @RepeatedTest(10)
+    public void createRandomTest(){
+        MovableStrategy movableStrategy = new MovableStrategy();
+        Integer randomNumber = movableStrategy.generateRandomNumber();
+        Assertions.assertThat(randomNumber).isBetween(0, 9);
+    }
+
+    @Test
+    public void moveIfNumberGreaterOrEqualThan4() {
+        MovableStrategy movableStrategy = new MovableStrategy();
+        int number = 4;
+        boolean result = movableStrategy.canMove(number);
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    public void notMoveIfNumberGreaterOrEqualThan4() {
+        MovableStrategy movableStrategy = new MovableStrategy();
+        int number = 3;
+        boolean result = movableStrategy.canMove(number);
+        Assertions.assertThat(result).isFalse();
+    }
+}
