30cc2319651c313059dfed8dfa244b00497833ae
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-28 14:38:24 +0900

    feat: StringCalculatorInputReader로 입력을 받는 로직으로 변경

--- src/main/java/stringcalc/StringCalculatorApp.java
+++ src/main/java/stringcalc/StringCalculatorApp.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class StringCalculatorApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        input = input.replaceAll("\\\\n", "\n");
+        StringCalculatorInputReader stringCalculatorInputReader = new StringCalculatorInputReader();
+        String input = stringCalculatorInputReader.read();
+
         StringCalculator stringCalculator = new StringCalculator();
         System.out.println(stringCalculator.calculate(input));
-        scanner.close();
+
+        stringCalculatorInputReader.close();
     }
 }
