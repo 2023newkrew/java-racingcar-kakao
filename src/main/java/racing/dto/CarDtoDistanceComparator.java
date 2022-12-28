febb993e3dd23618b658748cfd5d8523be262545
kyne.k <dd2307@snu.ac.kr>
2022-12-28 16:48:12 +0900

    feat(racing): CarDtoDistanceComparator 구현
    - CarDTO 간 거리 비교를 가능하도록 하는 Comparator 구현

---
+++ src/main/java/racing/dto/CarDtoDistanceComparator.java
@@ -0,0 +1,13 @@
+package racing.dto;
+
+import java.util.Comparator;
+
+public class CarDtoDistanceComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        if (!(o1 instanceof CarDTO) || !(o2 instanceof CarDTO)) {
+            throw new IllegalArgumentException("[ERROR] 인자 타입을 확인해주세요.");
+        }
+        return ((CarDTO)o1).getDistance() - ((CarDTO)o2).getDistance();
+    }
+}
