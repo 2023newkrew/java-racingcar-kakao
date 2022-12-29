692823fcc0151614874b6ae824f46578c9aab563
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-29 15:08:10 +0900

    refactor: @BeforeEach로 중복 제거

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,17 +12,23 @@ import static org.assertj.core.api.Assertions.*;
 
 class RacingGameTest {
 
-    @DisplayName("한 라운드를 진행하고 진행 결과로 RacingCar 리스트를 반환")
-    @Test
-    void playRound() {
-        //given
-        List<RacingCar> racingCars = Arrays.asList(
+    List<RacingCar> racingCars;
+    int rounds;
+    RacingGame racingGame;
+
+    @BeforeEach
+    void setUp() {
+        racingCars = Arrays.asList(
                 new RacingCar("car1"),
                 new RacingCar("car2"),
                 new RacingCar("car3"));
-        int rounds = 5;
-        RacingGame racingGame = new RacingGame(racingCars, rounds);
+        rounds = 5;
+        racingGame = new RacingGame(racingCars, rounds);
+    }
 
+    @DisplayName("한 라운드를 진행하고 진행 결과로 RacingCar 리스트를 반환")
+    @Test
+    void playRound() {
         //when
         List<RacingCar> roundResult = racingGame.playRound();
 
@@ -33,14 +39,6 @@ class RacingGameTest {
     @DisplayName("입력받은 라운드만큼 라운드를 진행")
     @Test
     void playRound_finish() {
-        //given
-        List<RacingCar> racingCars = Arrays.asList(
-                new RacingCar("car1"),
-                new RacingCar("car2"),
-                new RacingCar("car3"));
-        int rounds = 5;
-        RacingGame racingGame = new RacingGame(racingCars, rounds);
-
         //when
         int roundCnt = 0;
         while (!racingGame.isFinished()) {
@@ -56,7 +54,7 @@ class RacingGameTest {
     @DisplayName("남은 라운드가 없는데 라운드를 진행하면 예외 발생")
     @Test
     void playRound_throwEx() {
-        RacingGame racingGame = new RacingGame(new ArrayList<>(), 0);
+        racingGame = new RacingGame(new ArrayList<>(), 0);
 
         assertThatThrownBy(racingGame::playRound).isInstanceOf(IllegalStateException.class);
     }
