0b371e58957df1bccb47e16a87d96eecadbd8504
JongWoo Jeong <knight7024@naver.com>
2022-12-29 22:11:54 +0900

    test: 문자열 계산기의 테스트 추가 - 들어오는 문자열이 빈 문자열 혹은 null일 경우 검증

--- src/test/java/calculator/ParserTest.java
+++ src/test/java/calculator/ParserTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class ParserTest {
 
     @Test
-    @DisplayName("기본 구분자 - 정답")
-    void defaultDelimitersValid() {
+    @DisplayName("기본 구분자")
+    void validDefaultDelimiters() {
         String testString = "1,2:3";
         Parser calc = new Parser(testString);
         String[] result = calc.parse();
@@ -18,11 +18,29 @@ public class ParserTest {
     }
 
     @Test
-    @DisplayName("커스텀 구분자 분리 - 정답")
+    @DisplayName("커스텀 구분자 분리")
     void separateCustomDelimiter() {
         String testString = "//;\n1;2;3,4:5";
         Parser calc = new Parser(testString);
         String[] result = calc.parse();
         assertArrayEquals(result, new String[]{"1", "2", "3", "4", "5"});
+    }
+
+    @Test
+    @DisplayName("들어오는 문자열이 빈 문자열일 경우")
+    void validEmptyValue() {
+        String testString = "";
+        Parser calc = new Parser(testString);
+        String[] result = calc.parse();
+        assertArrayEquals(result, new String[]{"0"});
+    }
+
+    @Test
+    @DisplayName("들어오는 문자열이 null일 경우")
+    void validNullValue() {
+        String testString = null;
+        Parser calc = new Parser(testString);
+        String[] result = calc.parse();
+        assertArrayEquals(result, new String[]{"0"});
     }
 }
