e1fd2b0793d45d20096b1c1801ee8e24ba12a382
pyun0825 <pyun0825@naver.com>
2022-12-30 00:18:39 +0900

    test(racing): add StringParserTest

---
+++ src/test/java/car_racing/StringParserTest.java
@@ -0,0 +1,39 @@
+package car_racing;
+
+import car_racing.domain.StringParser;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class StringParserTest {
+    @Test
+    void parseTest_validInput() {
+        //given
+        String string = "a,b,c";
+        //when
+        String[] parsedStrings = StringParser.parse(string);
+        //then
+        Assertions.assertThat(parsedStrings).isEqualTo(new String[] {"a", "b", "c"});
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaaa,b,c", "a,,c"})
+    void parseTest_invalidInput(String string) {
+        //given
+        //when
+        //then
+        Assertions.assertThatRuntimeException().isThrownBy(() -> StringParser.parse(string));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void parseTest_nullAndEmpty(String string) {
+        //given
+        //when
+        //then
+        Assertions.assertThatRuntimeException().isThrownBy(() -> StringParser.parse(string));
+    }
+}
