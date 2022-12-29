61b65e1c62676393afbcee314303eca54a485087
Taehyun Kim <65909160+kimtaehyun98@users.noreply.github.com>
2022-12-29 11:28:24 +0900

    [hyun.k] 문자열 덧셈 계산기 & 자동차 경주 1단계 (#19)
    * feat(calc): TDD로 문자열 덧셈 계산기 구현
    
    * test(racing): Car이름 조건에 대한 테슽트
    
    * feat(racing): Car name에 대한 기능 구현
    
    * test(racing): Car location과 movement에 대한 테스트코드작성
    
    * feat(racing): Car location과 movement에 대한 기능 구현
    
    * test(racing): Car printLocation에 대한 테스트코드 추가
    
    * feat(racing): Car printLocation 기능 구현
    
    * fix(racing): CarTest에서 import * 제거 후 직접 import 로 수정
    
    * test(racing): Game init 테스트코드 작성
    
    * feat(racing): Game init 구현
    
    * test(racing): Game moveCars 구현
    
    * fix(racing): moveCars 테스트코드 수정
    
    * feat(racing): Game moveCars 코드 구현
    
    * test(racing): Game run 테스트 작성
    
    * feat(racing): Game run 기능구현
    
    * test(racing): Game maxLocation, getWinner 테스트 작성
    
    * feat(racing): Game maxLocation getWinner 구현
    
    * fix(racing): Game remove 대신 removeIf를 이용한 전체삭제
    
    * feat(racing): Main 클래스 main 함수 추가, 실행 가능한 함수로 구현
    
    * fix(racing): Game init에서 checkName 문제를 수정
    
    * doc(racing): Game, Car, Main의 주석을 추가함.
    
    * doc(racing): README 작성
    
    * fix(racing): 클래스 내부 메서드 this. 추가
    
    Co-authored-by: kane.woz <kane.woz@kakaocorp.com>

---
+++ src/test/java/com/racing/CarTest.java
@@ -0,0 +1,44 @@
+package com.racing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class CarTest {
+
+    // 차 이름이 5글자 이하인지에 대한 테스트
+    @Test
+    void checkNameTest(){
+        Car car = new Car("carName");
+
+        // RuntimeException 발생 시 종료
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> car.checkName());
+    }
+
+    @Test
+    void movementTest(){
+        Car car1 = new Car("car1");
+        // 기본 상태 0이여야 함
+        assertThat(car1.location).isEqualTo(0);
+        // 이동하지 못함(사유 : 입력이 0 임) 위치는 0임
+        car1.movement(0);
+        assertThat(car1.location).isEqualTo(0);
+        // 이동하지 못함(사유 : 입력이 3 임) 위치는 0임
+        car1.movement(3);
+        assertThat(car1.location).isEqualTo(0);
+        // 1만큼 이동함(사유 : 입력이 4 임) 위치는 1임
+        car1.movement(4);
+        assertThat(car1.location).isEqualTo(1);
+    }
+
+    // 차의 현재 location 출력 테스트
+    @Test
+    void printLocationTest(){
+        Car car1 = new Car("car1");
+        // 한 칸 이동
+        car1.movement(9);
+        assertThat(car1.printLocation()).isEqualTo("car1 : -");
+    }
+}
---
+++ src/test/java/com/racing/GameTest.java
@@ -0,0 +1,82 @@
+package com.racing;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class GameTest {
+
+    @Test
+    void initTest(){
+        Game game = new Game();
+        game.init("car1,car2,car3");
+        // init에서 3개의 자동차를 선언했으므로 길이가 3이여야 함
+        assertThat(game.cars).hasSize(3);
+        // 각 자동차는 이동거리가 0인 자동차이고 각각의 이름은 car1, car2, car3 이므로 아래와 같은 출력값을 가져야 한다.
+        assertThat(game.cars.get(0).printLocation()).isEqualTo("car1 : ");
+        assertThat(game.cars.get(1).printLocation()).isEqualTo("car2 : ");
+        assertThat(game.cars.get(2).printLocation()).isEqualTo("car3 : ");
+    }
+
+    // 모든 차 이동 (1턴)
+    @Test
+    void moveCarsTest(){
+        Game game = new Game();
+        game.init("car1,car2,car3");
+
+        ArrayList<Integer> rands = game.moveCars();
+
+        for(int i=0; i<3; i++){
+            String move = rands.get(i) < 4 ? "" : "-";
+            assertThat(game.cars.get(i).printLocation()).isEqualTo("car" + (i + 1) + " : " + move);
+        }
+    }
+
+    @Test
+    void runTest(){
+        Game game = new Game();
+        game.init("car1,car2,car3");
+        game.run(5);
+        // runCount는 run 메서드가 호출될때 사용된 n 횟수를 다 더한 값이다.
+        assertThat(game.runCount).isEqualTo(5);
+    }
+
+    @Test
+    void maxLocationTest(){
+        Game game = new Game();
+        game.init("car1,car2");
+        game.run(5);
+
+        // 자동차 location의 max 값 직접 구함
+        int findMax = Math.max(game.cars.get(0).location, game.cars.get(1).location);
+        assertThat(findMax).isEqualTo(game.maxLocation());
+    }
+
+    @Test
+    void getWinnerTest(){
+        Game game = new Game();
+        game.init("car1,car2");
+        game.run(5);
+
+        ArrayList<Car> winners = game.getWinner();
+
+        ArrayList<Car> cmpWinners = makeCmpWinners(game);
+
+        assertThat(winners).isEqualTo(cmpWinners);
+    }
+
+    public ArrayList<Car> makeCmpWinners(Game game){
+        ArrayList<Car> cmpWinners = new ArrayList<>();
+        int maxLocation = game.maxLocation();
+        if(game.cars.get(0).location == maxLocation){
+            cmpWinners.add(game.cars.get(0));
+        }
+        if(game.cars.get(1).location == maxLocation){
+            cmpWinners.add(game.cars.get(1));
+        }
+        return cmpWinners;
+    }
+}
