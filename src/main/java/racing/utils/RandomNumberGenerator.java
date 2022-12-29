608f13f25c871e84e93be777a32553da351469c7
kyne.k <dd2307@snu.ac.kr>
2022-12-29 11:24:52 +0900

    refactor(racing): RandomNumberGenerator 객체가 생성되지 못하도록 변경
    - private Constructor 생성
    - 생성자 호출시 Error 발생

--- src/main/java/racing/utils/RandomNumberGenerator.java
+++ src/main/java/racing/utils/RandomNumberGenerator.java
@@ -6,6 +6,9 @@ public class RandomNumberGenerator {
     private static final int LOWER_BOUND = 0;
     private static final int UPPER_BOUND = 9;
     private static final Random random = new Random();
+    private RandomNumberGenerator() {
+        throw new AssertionError();
+    }
 
     public static int generate() {
         return LOWER_BOUND + random.nextInt(UPPER_BOUND - LOWER_BOUND + 1);
--- src/test/java/racing/utils/UtilTest.java
+++ src/test/java/racing/utils/UtilTest.java
@@ -1,18 +1,12 @@
 package racing.utils;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilTest {
-    private RandomNumberGenerator generator;
-    @BeforeEach
-    void init() {
-        generator = new RandomNumberGenerator();
-    }
     @RepeatedTest(100)
     void randomNumberGenerateTest() {
-        assertThat(generator.generate()).isBetween(0.0, 9.0);
+        assertThat(RandomNumberGenerator.generate()).isBetween(0, 9);
     }
 }
