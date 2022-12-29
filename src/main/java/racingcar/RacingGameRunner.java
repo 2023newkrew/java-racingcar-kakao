89126dd966c7b0962b5709836cbec85e20b0411d
jeothen <jeothen@gmail.com>
2022-12-29 17:46:58 +0900

    refactor: Cars 클래스 삭제 - RacingGameRunner에 반영

--- src/main/java/racingcar/RacingGameRunner.java
+++ src/main/java/racingcar/RacingGameRunner.java
@@ -1,23 +1,71 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RacingGameRunner {
-    private final String carNames;
     private final int numberOfGames;
+    private final List<Car> carList;
 
     public RacingGameRunner(final String carNames, final int numberOfGames) {
-        this.carNames = carNames;
         this.numberOfGames = numberOfGames;
+        this.carList = registerCars(carNames);
     }
 
-    public void run() {
+    private List<Car> registerCars(final String carNames){
         InputParser inputParser = new InputParser();
+        List<String> splitCarNames = inputParser.splitNames(carNames);
 
-        Cars cars = new Cars(inputParser.splitNames(carNames));
+        return splitCarNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+
+    public void run() {
         for (int i = 0; i < numberOfGames; i++) {
-            cars.move();
-            cars.printCars();
+            this.move();
+            this.printCars();
             System.out.println();
         }
-        cars.printWinners();
+        this.printWinners();
+    }
+
+    public void move() {
+        for (Car car: carList){
+            car.move();
+        }
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public void printCars() {
+        for (Car car : carList) {
+            car.print();
+        }
+    }
+
+    public void printWinners() {
+        List<Car> winners = pickWinners();
+
+        List<String> winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        System.out.print(String.join(", ", winnerNames));
+        System.out.println("가 최종 우승했습니다.");
+    }
+
+    private List<Car> pickWinners(){
+        int maxPosition = carList.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return carList.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/racingcar/MoveCarTest.java
+++ src/test/java/racingcar/MoveCarTest.java
@@ -19,10 +19,10 @@ public class MoveCarTest {
 
     @RepeatedTest(10)
     public void multiCarsMove(){
-        List<String> carNames = Arrays.asList("Car1", "Car2", "Car3");
-        Cars cars = new Cars(carNames);
-        cars.move();
-        for (Car car: cars.getCarList()){
+        String carNames = "Car1,Car2,Car3";
+        RacingGameRunner racingGameRunner = new RacingGameRunner(carNames,1);
+        racingGameRunner.move();
+        for (Car car: racingGameRunner.getCarList()){
             Assertions.assertThat(car.getPosition()).isBetween(0, 1);
         }
     }
