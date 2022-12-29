5ef9f2a650ed2f0e1d88396c211f39113c008639
fbgnwhife1 <fbgnwhife@gamil.com>
2022-12-29 16:24:28 +0900

    test: 입력 예외 테스트 추가

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
+public class InputTest {
+
+    @DisplayName("null & empty 입력 예외 테스트")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void nullAndEmptyExceptionTest(String input){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            InputUI.validateCarList(Arrays.asList("123", "345", input));
+        });
+    }
+
+
+}
