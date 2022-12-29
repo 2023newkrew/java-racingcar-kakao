30e2ad307c1bf5e6c88c4cb05c4b25be590b1b1c
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2022-12-29 12:49:00 +0900

    refactor: view 수정

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,44 +1,45 @@
+//사용자에게 보여지는 부분을 적자
 package racingcar;
 
 import java.util.Scanner;
+import java.util.List;
 
 public class View {
-    Racing racing = new Racing();
+    //private Racing racing;
+    //View(Racing racing) {
+    //    this.racing = racing;
+    //}
 
-    public void input() {
+    public static List<String> inputCarNames() {
         Scanner sc = new Scanner(System.in);
-        String carNames;
+        String nameInput;
         do {
             System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-            carNames = sc.nextLine();
-        } while (!racing.registerCarNames(carNames));
+            nameInput = sc.nextLine();
+        } while(!Validate.validateCarNames(nameInput));
+        return Parser.stringArrayToStringList(nameInput.split(","));
+    }
 
-        String roundNum;
+    public static int inputRoundNum() {
+        Scanner sc = new Scanner(System.in);
+        String roundNumInput;
         do {
             System.out.println("시도할 회수는 몇회인가요?");
-            roundNum = sc.nextLine();
-        } while (!racing.registerCarRoundNum(roundNum));
+            roundNumInput = sc.nextLine();
+        } while (!Validate.validateRoundNumber(roundNumInput));
+        return Parser.stringToInt(roundNumInput);
     }
 
-    void runRound() {
-        for (int i = 0; i < racing.getRoundNum(); i++) {
-            racing.round();
-            String result = racing.roundResult();
-            printRoundResult(result);
+    public static void printRoundResult(List<Car> roundResult) {
+        for (Car car : roundResult) {
+            System.out.print(car.getName() + " : ");
+            System.out.println("-".repeat(car.getPosition()));
         }
+       System.out.println();
     }
 
-    void printRoundResult(String result) {
-        String[] temp = result.split(",");
-        for (int i = 0; i < temp.length; i += 2) {
-            System.out.print(temp[i] + " : ");
-            System.out.println("-".repeat(Integer.parseInt(temp[i + 1])));
-        }
-        System.out.println();
-    }
-
-    void printWinner() {
-        System.out.print(String.join(", ", racing.getWinner().split(",")));
+    public static void printWinner(List<String> winners) {
+        System.out.print(String.join(", ", winners));
         System.out.println("가 최종 우승했습니다.");
     }
 }
