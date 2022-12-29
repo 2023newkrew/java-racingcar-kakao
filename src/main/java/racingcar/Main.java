92d096847ff5eb74f5d7be97fbcaf72707a45a9b
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2022-12-29 13:16:20 +0900

    refactor: main문 수정

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -1,10 +1,16 @@
 package racingcar;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        View view = new View();
-        view.input();
-        view.runRound();
-        view.printWinner();
+        List<String> names = View.inputCarNames();
+        int roundNum = View.inputRoundNum();
+        Racing racing = new Racing(names, roundNum);
+        for(int i = 0; i<roundNum; i++) {
+            racing.round();
+            View.printRoundResult(racing.roundResult());
+        }
+        View.printWinner(racing.getWinner());
     }
 }
