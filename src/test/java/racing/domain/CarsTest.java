6a2eec0e08f784628c38283ab9f1bbf2684ce8c0
HQkim <hyunkyu001@gmail.com>
2022-12-29 14:34:39 +0900

    refactor(racing): CarsTest 리팩토링
    - 메소드 이름 한글로 변경

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -16,13 +16,13 @@ public class CarsTest {
     }
 
     @Test
-    void singleWinnerTest() {
+    void 한명의_우승자가_발생하는_경우() {
         cars.play(List.of(0.1, 0.3, 2.9, 8.8, 1.4));
         assertThat(cars.getWinners()).isEqualTo(Arrays.asList("cosmo"));
     }
 
     @Test
-    void multiWinnerTest() {
+    void 두명의_우승자가_발생하는_경우() {
         cars.play(List.of(0.1, 0.3, 2.9, 8.8, 4.4));
         cars.play(List.of(0.1, 5.3, 2.9, 7.0, 6.4));
         assertThat(cars.getWinners()).isEqualTo(Arrays.asList("cosmo", "kyne"));
