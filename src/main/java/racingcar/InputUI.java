d2e8abfa71fd0140e1ffae6b6048313cd4daa17d
fbgnwhife1 <fbgnwhife@gamil.com>
2022-12-29 17:19:53 +0900

    style: 줄 맞춤, {}

--- src/main/java/racingcar/InputUI.java
+++ src/main/java/racingcar/InputUI.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class InputUI {
     private static final Scanner sc = new Scanner(System.in);
 
-    public static List<String> inputCarNameList(){
+    public static List<String> inputCarNameList() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = sc.nextLine();
         checkNullAndEmpty(input);
@@ -17,7 +17,9 @@ public class InputUI {
     }
 
     public static List<String> validateCarList(List<String> carNameList) {
-        if(carNameList==null || carNameList.isEmpty()) throw new IllegalArgumentException("입력이 잘못 되었습니다.");
+        if (carNameList == null || carNameList.isEmpty()) {
+            throw new IllegalArgumentException("입력이 잘못 되었습니다.");
+        }
 
         for (String carName : carNameList) {
             checkNullAndEmpty(carName);
@@ -28,11 +30,13 @@ public class InputUI {
     }
 
     private static void checkNullAndEmpty(String car) {
-        if(car==null || car.isBlank()) throw new IllegalArgumentException("차 이름은 한 글자 이상이어야 합니다.");
+        if (car == null || car.isBlank()) {
+            throw new IllegalArgumentException("차 이름은 한 글자 이상이어야 합니다.");
+        }
     }
 
     private static void validateMaxLength(String car) {
-        if(car.length() > 5){
+        if (car.length() > 5) {
             throw new IllegalArgumentException("차 이름은 5글자 이하여야 합니다.");
         }
     }
@@ -45,7 +49,7 @@ public class InputUI {
         return Integer.parseInt(input);
     }
 
-    public static void validateTurn(String turn){
+    public static void validateTurn(String turn) {
         Matcher matcher = Pattern.compile("(\\d+)").matcher(turn);
         if (!matcher.matches()) {
             throw new IllegalArgumentException("숫자만 입력이 가능합니다.");
