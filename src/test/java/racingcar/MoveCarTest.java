d0060189ef7cd0a32b8f16c85902d8c806189739
jeothen <jeothen@gmail.com>
2022-12-29 19:41:11 +0900

    refactor: 불필요한 TC 및 메소드 삭제

--- src/test/java/racingcar/MoveCarTest.java
+++ src/test/java/racingcar/MoveCarTest.java
@@ -15,17 +15,6 @@ public class MoveCarTest {
         car.moveOne(canMove);
         Assertions.assertThat(car.getPosition()).isEqualTo(1);
     }
-
-    @RepeatedTest(10)
-    public void multiCarsMove(){
-        String carNames = "Car1,Car2,Car3";
-        RacingGameRunner racingGameRunner = new RacingGameRunner();
-        racingGameRunner.registeredData(carNames, 1);
-        racingGameRunner.move();
-        for (Car car: racingGameRunner.getCarList()){
-            Assertions.assertThat(car.getPosition()).isBetween(0, 1);
-        }
-    }
 }
 
 
