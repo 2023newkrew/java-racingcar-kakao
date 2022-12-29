242274c166b0f13f53760844122f62608e919ef2
jin.100 <jin.100@kakaocorp.com>
2022-12-29 16:09:35 +0900

    refactor : RacingGameRunner 를 RacingGameController 로 rename하고 게임 동작 로직을 모음

--- src/main/java/racingcar/RacingGameMain.java
+++ src/main/java/racingcar/RacingGameMain.java
@@ -1,18 +1,11 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
 import racingcar.view.RacingGameInputView;
-
-import java.util.Scanner;
 
 public class RacingGameMain {
 
     public static void main(String[] args) {
-        RacingGameInputView racingGameInputView = new RacingGameInputView();
-
-        String carNames = racingGameInputView.getCarNames();
-        Integer numberOfGames = racingGameInputView.getNumberOfGames();
-
-        RacingGameRunner racingGame = new RacingGameRunner(carNames, numberOfGames);
-        racingGame.run();
+        new RacingGameController().run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,30 @@
+package racingcar.controller;
+
+import racingcar.Cars;
+import racingcar.InputParser;
+import racingcar.view.RacingGameInputView;
+import racingcar.view.RacingGameOutputView;
+
+public class RacingGameController {
+    private final RacingGameInputView racingGameInputView;
+    private final RacingGameOutputView racingGameOutputView;
+
+    public RacingGameController() {
+        this.racingGameInputView = new RacingGameInputView();
+        this.racingGameOutputView = new RacingGameOutputView();
+    }
+
+    public void run() {
+        String carNames = racingGameInputView.getCarNames();
+        Integer numberOfGames = racingGameInputView.getNumberOfGames();
+
+        InputParser inputParser = new InputParser();
+
+        Cars cars = new Cars(inputParser.splitNames(carNames));
+        for (int i = 0; i < numberOfGames; i++) {
+            cars.move();
+            racingGameOutputView.printCars(cars);
+        }
+        racingGameOutputView.printWinners(cars.findWinners());
+    }
+}
