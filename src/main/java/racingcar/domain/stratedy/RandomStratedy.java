0442c46a8e78ddd9067e69f3abc1229a83e71ed4
_Min <secured8372@gmail.com>
2022-12-29 15:19:12 +0900

    refactor: 상수를 모은 Constants 클래스 추가
    상수를 모은 Constants 클래스 추가
     - RandomStratedy 클래스에서 해당 클래스 사용하도록 변경

--- src/main/java/racingcar/domain/stratedy/RandomStratedy.java
+++ src/main/java/racingcar/domain/stratedy/RandomStratedy.java
@@ -1,11 +1,11 @@
 package racingcar.domain.stratedy;
 
+import racingcar.domain.Constants;
+
 import java.util.Random;
 
 public class RandomStratedy implements MovableStrategy {
-    private final int FORWARD_NUM = 4;
-
     public boolean isMove() {
-        return new Random().nextInt(10) >= FORWARD_NUM;
+        return new Random().nextInt(Constants.RANDOM_MAX_NUM) >= Constants.FORWARD_NUM;
     }
 }
