385ac320b654fbe0d80b89be92c03a89488b8afd
jin.100 <jin.100@kakaocorp.com>
2022-12-29 15:39:51 +0900

    refactor : 사용자 입력 받는 부분 RacingGameInputView 클래스로 이동

---
+++ src/main/java/racingcar/view/RacingGameInputView.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class RacingGameInputView {
+    private final Scanner scanner;
+
+    public RacingGameInputView() {
+        this(new Scanner(System.in));
+    }
+
+    public RacingGameInputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+
+    public String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
+
+    public Integer getNumberOfGames() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return scanner.nextInt();
+    }
+}
