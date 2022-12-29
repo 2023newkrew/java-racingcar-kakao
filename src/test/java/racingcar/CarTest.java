5c13ab8306f5b52134f1c67ad98e34294c21fe81
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2022-12-29 14:41:58 +0900

    test: 테스트명 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    public void 랜덤_값이_4_이상일_경우_한_칸_전진한다(final int condition) {
+    public void 값이_4_이상일_경우_한_칸_전진한다(final int condition) {
         Car car = new Car("aaaa");
         assertEquals(car.move(condition), 1);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
-    public void 랜덤_값이_3_이하일_경우_정지한다(final int condition) {
+    public void 값이_3_이하일_경우_정지한다(final int condition) {
         Car car = new Car("aaaa");
         assertEquals(car.move(condition), 0);
     }
