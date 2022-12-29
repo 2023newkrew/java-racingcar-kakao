41bbd073e5b5b2bde4c2a3589d3a5fc35ebccd99
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-29 14:56:32 +0900

    feat: 모든 라운드를 진행시키는 로직을 한 라운드만 진행시키게 변경

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,47 +1,17 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
 
-    private final RacingGameScoreView racingGameScoreView;
     private final RandomMovementManager randomMovementManager;
-    private final RacingGameReferee racingGameReferee;
-
     private final List<RacingCar> racingCars;
     private int remainingRounds;
 
-    public RacingGame() {
-        this.racingGameScoreView = new RacingGameScoreView();
-        this.randomMovementManager = new RandomMovementManager(new RandomNumberGeneratorImpl());
-        this.racingGameReferee = new RacingGameReferee();
-        this.racingCars = new ArrayList<>();
-        this.remainingRounds = 0;
-    }
-
     public RacingGame(List<RacingCar> racingCars, int remainingRounds) {
-        this.racingGameScoreView = new RacingGameScoreView();
         this.randomMovementManager = new RandomMovementManager(new RandomNumberGeneratorImpl());
-        this.racingGameReferee = new RacingGameReferee();
         this.racingCars = racingCars;
         this.remainingRounds = remainingRounds;
-    }
-
-    public void play(List<RacingCar> racingCars, int rounds) {
-        System.out.println("\n실행 결과");
-        racingGameScoreView.printRacingCarDist(racingCars);
-        for (int round = 0; round < rounds; round++) {
-            playRound(racingCars);
-            racingGameScoreView.printRacingCarDist(racingCars);
-        }
-        racingGameScoreView.printWinners(racingGameReferee.findWinners(racingCars));
-    }
-
-    private void playRound(List<RacingCar> racingCars) {
-        for (RacingCar racingCar : racingCars) {
-            racingCar.move(randomMovementManager.makeMovementDecision());
-        }
     }
 
     public List<RacingCar> playRound() {
