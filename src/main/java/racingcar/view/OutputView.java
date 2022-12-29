04bb69cf90537ad9b8fdd4d3ac8545f4d04e9912
rlfalsgh95 <rlfalsgh95@naver.com>
2022-12-29 18:24:06 +0900

    refactor : OutputView 문자의 상수화 및 출력 문자열의 formatting

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,6 +8,8 @@ import racingcar.model.Positions;
 
 public class OutputView {
 
+    public static final String PROGRESS_BAR_ELEM = "-";
+    public static final String CAR_NAME_DELIMITER = ", ";
     private final PrintStream printStream;
 
     public OutputView(PrintStream printStream) {
@@ -22,20 +24,20 @@ public class OutputView {
     public void printPositions(Positions positions) {
         for (String name : positions.getNames()) {
             String progressBar = progressBar(positions, name);
-            printStream.println(name + " : " + progressBar);
+            printStream.printf("%s : %s", name, progressBar);
         }
         printStream.println();
     }
 
     private String progressBar(Positions positions, String name) {
-        return "-".repeat(positions.getPositionByName(name));
+        return PROGRESS_BAR_ELEM.repeat(positions.getPositionByName(name));
     }
 
     public void printWinners(List<Car> winners) {
         String winnersName = winners.stream()
                 .map(Car::getName)
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(CAR_NAME_DELIMITER + " "));
 
-        printStream.println(winnersName + "가 최종 우승했습니다.");
+        printStream.printf("%s 가 최종 우승했습니다.", winnersName);
     }
 }
