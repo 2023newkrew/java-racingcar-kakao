f1488a7537d49be62654bccd492bc0553645e162
jeothen <jeothen@gmail.com>
2022-12-29 17:46:15 +0900

    refactor: 자동차 이름이 5자를 넘는 경우 제외하는 코드 추가

--- src/main/java/racingcar/InputParser.java
+++ src/main/java/racingcar/InputParser.java
@@ -1,12 +1,17 @@
 package racingcar;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputParser {
     private static final int CAR_NAME_MAX_LENGTH = 5;
 
     public List<String> splitNames(String inputString){
-        return List.of(inputString.split(","));
+        List<String> nameList = List.of(inputString.split(","));
+
+        return nameList.stream()
+                .filter(this::checkCarNameLength)
+                .collect(Collectors.toList());
     }
 
     public boolean checkCarNameLength(String testString) {
