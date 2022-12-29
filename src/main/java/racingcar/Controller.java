aaa1892020116c62e3f6ae107b199f9919d16a76
Eithea <oguraff94@gmail.com>
2022-12-29 19:50:26 +0900

    separate Input and Output

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -2,25 +2,29 @@ package racingcar;
 
 import racingcar.model.RacingGame;
 import racingcar.view.IO;
+import racingcar.view.Input;
+import racingcar.view.Output;
 
 public class Controller {
 
-    private IO io;
+    private Input input;
+    private Output output;
 
     public Controller() {
-        this.io = new IO();
+        this.input = new Input();
+        this.output = new Output();
     }
 
     public void start() {
-        RacingGame game = new RacingGame(io.inputCarNames());
-        int turnIteration = io.inputTurnIteration();
+        RacingGame game = new RacingGame(input.getCarNames());
+        int turnIteration = input.getTurnIteration();
 
-        io.outputGameResultMessage();
-        io.outputTurnResult(game.getCars());
+        output.sendGameResultMessage();
+        output.sendTurnResult(game.getCars());
         for (int i = 0; i < turnIteration; i++) {
             game.proceedTurn();
-            io.outputTurnResult(game.getCars());
+            output.sendTurnResult(game.getCars());
         }
-        io.outputWinners(game.getFarthestCars());
+        output.sendWinners(game.getFarthestCars());
     }
 }
