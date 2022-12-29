cb9993c2fa2f325712b00a232be65d31597762f7
_Min <secured8372@gmail.com>
2022-12-29 13:23:55 +0900

    refactor: View 클래스를 정적으로 변경
    View 클래스를 정적으로 변경

--- src/main/java/racingcar/App.java
+++ src/main/java/racingcar/App.java
@@ -4,20 +4,24 @@ import racingcar.domain.Car;
 import racingcar.domain.Racing;
 import racingcar.view.RacingCarView;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
-        RacingCarView racingCarView = new RacingCarView();
         Racing racing = new Racing();
 
-        racing.addCars(racingCarView.inputCarNames().stream().map(Car::new).collect(Collectors.toList()));
-        racing.setCount(racingCarView.inputCount());
-        racingCarView.resultMessage();
-        while (racing.isNotFinished()) {
-            racing.tryForward();
-            racingCarView.printResultOfCars(racing.getCarList());
-        }
-        racingCarView.printWinners(racing.winner());
+        racing.addCars(stringToCar(RacingCarView.inputCarNames()));
+        racing.setCount(RacingCarView.inputCount());
+        RacingCarView.resultMessage();
+        while (racing.isNotFinished())
+            RacingCarView.printResultOfCars(racing.tryForward());
+        RacingCarView.printWinners(racing.winner());
+    }
+
+    private static List<Car> stringToCar(List<String> strings) {
+        return strings.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class RacingCarView {
 
-    public List<String> inputCarNames() throws RuntimeException {
+    public static List<String> inputCarNames() throws RuntimeException {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉽표(,)를 기준으로 구분).");
         Scanner sc = new Scanner(System.in);
         List<String> names = Arrays.asList(sc.nextLine().split(","));
@@ -17,35 +17,35 @@ public class RacingCarView {
         return names;
     }
 
-    private void checkCarName(List<String> names) throws RuntimeException {
+    private static void checkCarName(List<String> names) throws RuntimeException {
         names.forEach(s -> {
             if (s == null || s.length() == 0) throw new IllegalArgumentException();
             if (s.length() > 5) throw new IllegalArgumentException();
         });
     }
 
-    public int inputCount() {
+    public static int inputCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         return (new Scanner(System.in)).nextInt();
     }
 
-    public void printResultOfCars(List<Car> cars) {
-        cars.forEach(this::printResultOfACar);
+    public static void printResultOfCars(List<Car> cars) {
+        cars.forEach(RacingCarView::printResultOfACar);
         System.out.println();
     }
 
-    private void printResultOfACar(Car car) {
+    private static void printResultOfACar(Car car) {
         System.out.print(car.getName() + " : ");
         for (int i = 0; i < car.getDistance(); i++)
             System.out.print("-");
         System.out.println();
     }
 
-    public void resultMessage() {
+    public static void resultMessage() {
         System.out.println("실행 결과");
     }
 
-    public void printWinners(List<String> winners) {
+    public static void printWinners(List<String> winners) {
         StringBuilder sb = new StringBuilder();
         winners.forEach(w -> sb.append(w).append(", "));
         sb.delete(sb.length() - 2, sb.length() - 1);
