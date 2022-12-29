c590e7690fa0f5a74291cfcbbbc00dece2003add
rlfalsgh95 <rlfalsgh95@naver.com>
2022-12-29 17:12:25 +0900

    refactor : InputView의 자동차 이름 길이 validation 로직 Car 클래스로 이동

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,10 +3,16 @@ package racingcar.model;
 public class Car {
 
     public static final int MOVE_THRESHOLD = 4;
+    public static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
     private int position;
 
     public Car(String name) {
+        if(hasOverLengthName(name)){
+            throw new IllegalArgumentException("자동차 이름은" + MAX_NAME_LENGTH + " 이하여야 합니다.");
+        }
+
         this.name = name;
         this.position = 0;
     }
@@ -15,6 +21,10 @@ public class Car {
         if (isMovable(number)) {
             position++;
         }
+    }
+
+    private boolean hasOverLengthName(String name) {
+        return name.length() > MAX_NAME_LENGTH;
     }
 
     private boolean isMovable(int number) {
