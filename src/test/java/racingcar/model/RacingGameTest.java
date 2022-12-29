6dd08851af573ea7e1ca0a597c95ad02eb896343
rlfalsgh95 <rlfalsgh95@naver.com>
2022-12-29 14:17:33 +0900

    refactor(RacingGameTest.java) : 0인지 1인지 확인하는 로직 변경, 람다 -> 메서드 레퍼런스

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -34,7 +34,7 @@ class RacingGameTest {
         racingGame.moveCars();
         racingGame.moveCars();
 
-        assertThatThrownBy(() -> racingGame.moveCars())
+        assertThatThrownBy(racingGame::moveCars)
                 .isInstanceOf(IllegalStateException.class);
     }
 
@@ -48,7 +48,7 @@ class RacingGameTest {
         racingGame.moveCars();
 
         Positions positions = racingGame.getPositions();
-        assertThat(positions.getPositionByName("a")).isEqualTo(1);
+        assertThat(positions.getPositionByName("a")).isOne();
     }
 
     @DisplayName("특정 값 이하인 경우 자동차를 정지시킨다")
@@ -61,7 +61,7 @@ class RacingGameTest {
         racingGame.moveCars();
 
         Positions positions = racingGame.getPositions();
-        assertThat(positions.getPositionByName("a")).isEqualTo(0);
+        assertThat(positions.getPositionByName("a")).isZero();
     }
 
     @DisplayName("여러 대의 자동차를 이동 혹은 정지시킨다.")
@@ -73,9 +73,9 @@ class RacingGameTest {
         racingGame.moveCars();
 
         Positions positions = racingGame.getPositions();
-        assertThat(positions.getPositionByName("a")).isEqualTo(1);
-        assertThat(positions.getPositionByName("b")).isEqualTo(1);
-        assertThat(positions.getPositionByName("c")).isEqualTo(0);
+        assertThat(positions.getPositionByName("a")).isOne();
+        assertThat(positions.getPositionByName("b")).isOne();
+        assertThat(positions.getPositionByName("c")).isZero();
     }
 
     @DisplayName("가장 멀리 이동한 차가 단독 우승한다")
@@ -132,7 +132,7 @@ class RacingGameTest {
 
         racingGame.moveCars();
 
-        assertThatThrownBy(() -> racingGame.getWinners())
+        assertThatThrownBy(racingGame::getWinners)
                 .isInstanceOf(IllegalStateException.class);
     }
 }
