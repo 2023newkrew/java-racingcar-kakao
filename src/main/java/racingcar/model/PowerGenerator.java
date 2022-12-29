4fc6eda62f4203cfdcec0fe36b143a0e6574d3e5
Eithea <oguraff94@gmail.com>
2022-12-29 17:49:18 +0900

    apply review : replace Math to Random

--- src/main/java/racingcar/model/PowerGenerator.java
+++ src/main/java/racingcar/model/PowerGenerator.java
@@ -1,10 +1,16 @@
 package racingcar.model;
 
+import java.util.Random;
+
 public class PowerGenerator {
     public static final int POWER_LIMIT = 10;
+    private Random random;
+
+    public PowerGenerator() {
+        this.random = new Random();
+    }
 
     public int getRandomPower() {
-        return (int) (Math.random() * PowerGenerator.POWER_LIMIT)
-                % PowerGenerator.POWER_LIMIT;
+        return (int) random.nextInt(PowerGenerator.POWER_LIMIT);
     }
 }
