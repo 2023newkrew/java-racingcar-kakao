af15337aceaa502e446dad2a94af771c372d9ecb
pluto.yun <pluto.yun@kakaocorp.com>
2022-12-29 14:31:50 +0900

    refactor: StringSplitter 코드 정리
    - 개행
    - 중괄호 컨벤션 통일
    - separators에 final 키워드 추가
    - 인자 검증을 정적 팩토리 메소드로 이관
    - 함수정리

--- src/main/java/stringaccumulator/StringSplitter.java
+++ src/main/java/stringaccumulator/StringSplitter.java
@@ -2,27 +2,34 @@ package stringaccumulator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class StringSplitter {
 
     private static final List<String> DEFAULT_SEPARATORS = List.of(",", ":");
+
     private static final String PREFIX = "//";
+
     private static final String SUFFIX = "\n";
+
     private static final int PREFIX_LENGTH = PREFIX.length();
+
     private static final int SUFFIX_LENGTH = SUFFIX.length();
 
-    List<String> separators;
+    private final List<String> separators;
 
     public static StringSplitter from(String separatorFormat) {
-        return new StringSplitter(separatorFormat);
+        if (isNullOrBlank(separatorFormat)) {
+            return new StringSplitter(DEFAULT_SEPARATORS);
+        }
+        checkFormatValidation(separatorFormat);
+        String customSeparator = parseCustomSeparator(separatorFormat);
+        List<String> separators = gerSeparatorsWith(customSeparator);
+        return new StringSplitter(separators);
     }
 
-    private StringSplitter(String separatorFormat) {
-        separators = new ArrayList<>(DEFAULT_SEPARATORS);
-        if (separatorFormat == null)
-            return;
-        checkFormatValidation(separatorFormat);
-        addCustomSeparator(separatorFormat);
+    private static boolean isNullOrBlank(String separatorFormat) {
+        return Objects.isNull(separatorFormat) || separatorFormat.isBlank();
     }
 
     private static void checkFormatValidation(String separatorFormat) {
@@ -35,13 +42,25 @@ public class StringSplitter {
         return !separatorFormat.startsWith(PREFIX) || !separatorFormat.endsWith(SUFFIX);
     }
 
-    private void addCustomSeparator(String separatorFormat) {
+    private static String parseCustomSeparator(String separatorFormat) {
+        checkFormatValidation(separatorFormat);
         int lengthWithoutSuffix = separatorFormat.length() - SUFFIX_LENGTH;
-        String customSeparator = separatorFormat.substring(PREFIX_LENGTH, lengthWithoutSuffix);
-        if (!customSeparator.isEmpty())
-            separators.add(0, customSeparator);
+        return separatorFormat.substring(PREFIX_LENGTH, lengthWithoutSuffix);
     }
 
+    private static List<String> gerSeparatorsWith(String customSeparator) {
+        List<String> separators = new ArrayList<>(DEFAULT_SEPARATORS);
+        if (!customSeparator.isBlank()) {
+            separators.add(customSeparator);
+        }
+        return separators;
+    }
+
+    private StringSplitter(List<String> separators) {
+        this.separators = separators;
+    }
+
+    //Todo: 테스트에서만 사용하는 메소드 제거
     public boolean contains(String separator) {
         return separators.contains(separator);
     }
