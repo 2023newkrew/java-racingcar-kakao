c676b34a7965899931a59b05f0e4aba2e665bd11
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2022-12-29 17:20:45 +0900

    test: 각 테스트에 DisplayName으로 설명 추가

--- src/test/java/CarRacing/domain/CarTest.java
+++ src/test/java/CarRacing/domain/CarTest.java
@@ -1,6 +1,7 @@
 package CarRacing.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -19,22 +20,27 @@ public class CarTest {
         positionCar = new Car("test", 5);
     }
 
+    @DisplayName("랜덤한 숫자가 0부터 9사이로 생성되는지 테스트")
     @Test
     public void createRandomNumberTest() {
         assertThat(car.createRandomNumber()).isBetween(0, 9);
     }
 
+    @DisplayName("랜덤 넘버가 0이상 3이하 일때 자동차가 움직이지 않는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
     public void isNotMoveTest(int randomNumber) {
         assertThat(car.isMove(randomNumber)).isEqualTo(false);
     }
+
+    @DisplayName("랜덤 넘버가 4이상 9이하 일때 자동차가 움직이는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     public void isMoveTest(int randomNumber) {
         assertThat(car.isMove(randomNumber)).isEqualTo(true);
     }
 
+    @DisplayName("자동차 전진이 제대로 작동하는지 테스트")
     @ParameterizedTest
     @CsvSource({"true, 6", "false, 5"})
     public void moveCarTest(boolean move, int position) {
@@ -42,12 +48,14 @@ public class CarTest {
         assertThat(positionCar.getPosition()).isEqualTo(position);
     }
 
+    @DisplayName("자동차의 포지션과 최대 포지션의 비교가 잘 이루어지는지 테스트")
     @ParameterizedTest
     @CsvSource({"6, 6", "4, 5", "5, 5"})
     public void getMaxPositionTest(int maxPosition, int newMaxPosition) {
         assertThat(positionCar.getMaxPosition(maxPosition)).isEqualTo(newMaxPosition);
     }
 
+    @DisplayName("우승자면 이름을 반환하는지 테스트")
     @ParameterizedTest
     @CsvSource({"5, test", "6,"})
     public void getWinnerNameTest(int maxPosition, String winnerName) {
--- src/test/java/CarRacing/domain/RacingTest.java
+++ src/test/java/CarRacing/domain/RacingTest.java
@@ -1,6 +1,7 @@
 package CarRacing.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -20,20 +21,28 @@ public class RacingTest {
         cars = new Car[] {new Car("pobi", 5), new Car("crong", 4), new Car("honux", 5)};
     }
 
+    @DisplayName("입력 문자열을 잘 스플릿하는지 테스트")
     @Test
     public void splitInputStringTest() {
         String inputString = "pobi,crong,honux";
         assertThat(racing.splitInputString(inputString)).isEqualTo(new String[] {"pobi", "crong", "honux"});
     }
 
+    @DisplayName("적절한 길이(1이상 5이하)의 자동차 이름 예외 처리 테스트")
     @Test
-    public void checkCarNameLengthTest() {
-        String wrong_name = "pororo";
+    public void checkCorrectCarNameLengthTest() {
         String correct_name = "pobi";
-        assertThrows(RuntimeException.class, () -> {racing.checkCarNameLength(wrong_name);});
         assertDoesNotThrow(() -> {racing.checkCarNameLength(correct_name);});
     }
 
+    @DisplayName("적절하지 않은 길이(0, 6이상)의 자동차 이름 예외 처리 테스트")
+    @Test
+    public void checkWrongCarNameLengthTest() {
+        String wrong_name = "pororo";
+        assertThrows(RuntimeException.class, () -> {racing.checkCarNameLength(wrong_name);});
+    }
+
+    @DisplayName("이름 배열로 자동차 배열을 생성한 후 길이가 일치하는지 테스트")
     @Test
     public void makeCarListTest() {
         String[] names = new String[] {"pobi", "crong", "honux"};
@@ -41,16 +50,19 @@ public class RacingTest {
         assertThat(cars.length).isEqualTo(names.length);
     }
 
+    @DisplayName("자동차 배열에서 최대 포지션을 제대로 구하는지 테스트")
     @Test
     public void getMaxPositionTest() {
         assertThat(racing.getMaxPosition(cars)).isEqualTo(5);
     }
 
+    @DisplayName("자동차 배열에서 우승자를 제대로 구하는지 테스트")
     @Test
     public void getWinnersTest() {
         assertThat(racing.getWinners(cars)).isEqualTo(Arrays.asList("pobi", "honux"));
     }
 
+    @DisplayName("우승자 목록이 제대로 갱신되는지 테스트")
     @Test
     public void addWinnerTest() {
         List<String> winners = new ArrayList<>();
