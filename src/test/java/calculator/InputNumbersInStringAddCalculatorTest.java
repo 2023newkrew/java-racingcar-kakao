86934152513940d0153049575b754b0abb3491e8
HQkim <hyunkyu001@gmail.com>
2022-12-29 13:39:32 +0900

    refactor(calculator): InputNumbersInStringAddCalculatorTest 리팩터링
    - nullTest()와 emptyTest()를 하나의 테스트로 합침
    - 테스트명을 한글로 작성하여 테스트를 명확히 파악할 수 있도록 수정

--- src/test/java/calculator/InputNumbersInStringAddCalculatorTest.java
+++ src/test/java/calculator/InputNumbersInStringAddCalculatorTest.java
@@ -2,62 +2,59 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputNumbersInStringAddCalculatorTest {
-    @Test
-    void nullTest() {
-        InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator(null);
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void NULL_이거나_빈문자열이면_계산결과는_0이다 (String input) {
+        InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator(input);
         assertThat(calculator.calculate()).isEqualTo(0);
     }
 
     @Test
-    void emptyTest() {
-        InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("");
-        assertThat(calculator.calculate()).isEqualTo(0);
-    }
-
-    @Test
-    void oneNumberTest() {
+    void 구분자_없이_숫자만_계산이_잘되는지_확인() {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("1");
         assertThat(calculator.calculate()).isEqualTo(1);
     }
     @Test
-    void commaTest() {
+    void 콤마로_구분된_숫자가_덧셈이_잘되는지_확인() {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("1,2");
         assertThat(calculator.calculate()).isEqualTo(3);
     }
 
     @Test
-    void colonTest() {
+    void 콜론으로_구분된_숫자가_덧셈이_잘되는지_확인() {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("1:2");
         assertThat(calculator.calculate()).isEqualTo(3);
     }
 
     @Test
-    void commaColonTest() {
+    void 콤마와_콜론으로_구분된_숫자가_덧셈이_잘되는지_확인() {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("1,2:3");
         assertThat(calculator.calculate()).isEqualTo(6);
     }
 
     @Test
-    void customDelimiterTest() {
+    void 커스텀구분자로_구분된_숫자가_덧셈이_잘되는지_확인() {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("//;\n1;2");
         assertThat(calculator.calculate()).isEqualTo(3);
     }
 
     @Test
-    void allDelimiterTest() {
+    void 콤마_콜론_커스텀구분자로_구분된_숫자의_덧셈이_잘되는지_확인() {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator("//A\n1,2:3A4");
         assertThat(calculator.calculate()).isEqualTo(10);
     }
 
     @ValueSource(strings = {"2.3", "ab9", "1e9"})
     @ParameterizedTest
-    void inputTypeExceptionTest(String input) {
+    void 정수가_아닌_문자열이_주어졌을_경우_예외가_발생한다(String input) {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator(input);
         assertThatThrownBy(calculator::calculate)
                 .isInstanceOf(RuntimeException.class);
@@ -65,7 +62,7 @@ public class InputNumbersInStringAddCalculatorTest {
 
     @ValueSource(strings = {"-3", "-7:124", "//;\n1;2:-3"})
     @ParameterizedTest
-    void inputRangeExceptionTest(String input) {
+    void 음수인_정수가_입력되면_예외가_발생한다(String input) {
         InputNumbersInStringAddCalculator calculator = new InputNumbersInStringAddCalculator(input);
         assertThatThrownBy(calculator::calculate)
                 .isInstanceOf(RuntimeException.class);
