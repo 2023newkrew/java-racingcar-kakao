ba8a1faab2e94c0e9d2db76b2c65aac3c831e818
gh1224 <rlarkgus0@naver.com>
2022-12-30 15:08:05 +0900

    refactor: 리뷰 반영하여 자동차 경주 리팩토링

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,9 +7,45 @@ public class Game {
     private int curTurn = 0;
     private final Car[] cars;
 
+    public Game(int totalTurns, String[] names) {
+        this(totalTurns, namesToCars(names));
+    }
+
     public Game(int totalTurns, Car[] cars) {
         this.totalTurns = totalTurns;
         this.cars = cars;
+    }
+
+    public void play() {
+        for (int i = 0; i < cars.length; i++) {
+            moveCar(i, RandomNumGenerator.generateRandomNum());
+        }
+        printCars();
+        curTurn++;
+    }
+
+    public boolean isEnd() {
+        return curTurn == totalTurns;
+    }
+
+    public void printCars() {
+        for (Car car : cars) {
+            car.print();
+        }
+        System.out.println();
+    }
+
+    public void printWinners() {
+        Car[] winners = this.findWinners();
+        System.out.printf("%s가 최종 우승했습니다.\n", Game.carsToNames(winners));
+    }
+
+    public Car[] findWinners() {
+        ArrayList<Car> winnersList = new ArrayList<>();
+        for (Car car : cars) {
+            updateWinnerListWithCar(winnersList, car);
+        }
+        return winnersList.toArray(new Car[winnersList.size()]);
     }
 
     public void moveCar(int carIdx, int randomNumber) {
@@ -37,44 +73,20 @@ public class Game {
         return car.getPosition() == winners.get(0).getPosition();
     }
 
-    public Car[] findWinners() {
-        ArrayList<Car> winnersList = new ArrayList<>();
-        for (Car car: cars) {
-            updateWinnerListWithCar(winnersList, car);
+    private static Car[] namesToCars(String[] names) {
+        Car[] cars = new Car[names.length];
+        for (int i = 0; i < names.length; i++) {
+            cars[i] = new Car(names[i]);
         }
-        return winnersList.toArray(new Car[winnersList.size()]);
-    }
-
-    public void play() {
-        for (int i = 0; i < cars.length; i++) {
-            moveCar(i, RandomNumGenerator.generateRandomNum());
-        }
-        printCars();
-        curTurn++;
-    }
-
-    public boolean isEnd() {
-        return curTurn == totalTurns;
-    }
-
-    public void printCars() {
-        for (Car car : cars) {
-            car.print();
-        }
-        System.out.println();
+        return cars;
     }
 
     private static String carsToNames(Car[] cars) {
         StringBuilder sb = new StringBuilder();
-        for (Car car: cars) {
+        for (Car car : cars) {
             sb.append(car.getName()).append(',');
         }
-        sb.deleteCharAt(sb.length()-1);
+        sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
-    }
-
-    public void printWinners() {
-        Car[] winners = this.findWinners();
-        System.out.printf("%s가 최종 우승했습니다.\n", Game.carsToNames(winners));
     }
 }
--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -3,33 +3,10 @@ package racingcar;
 import java.util.Scanner;
 
 public class Main {
-    public static Car[] namesToCars(String input) {
-        String[] names = input.split(",");
-        Car[] cars = new Car[names.length];
-        for (int i = 0; i < names.length; i++) {
-            cars[i] = new Car(names[i]);
-        }
-        return cars;
-    }
-
-    private static Car[] getCars(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carNameString = sc.nextLine();
-        return Main.namesToCars(carNameString);
-    }
-
-    private static int getTotalTurns() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("시도할 회수는 몇회인가요?");
-        int totalTurns = sc.nextInt();
-        return totalTurns;
-    }
-
     public static void main(String[] args) {
-        Car[] cars = getCars();
-        int totalTurns = getTotalTurns();
-        Game game = new Game(totalTurns, cars);
+        String carNameStr = scanCarNameStr();
+        int totalTurns = scanTotalTurns();
+        Game game = new Game(totalTurns, carNameStr.split(","));
 
         System.out.println("\n실행 결과");
         game.printCars();
@@ -37,5 +14,19 @@ public class Main {
             game.play();
         }
         game.printWinners();
+    }
+
+    private static String scanCarNameStr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNameStr = sc.nextLine();
+        return carNameStr;
+    }
+
+    private static int scanTotalTurns() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        int totalTurns = sc.nextInt();
+        return totalTurns;
     }
 }
