7652625b696d509bf19c118220dda9b400a1da04
jeothen <jeothen@gmail.com>
2022-12-30 12:22:53 +0900

    refactor: 예외처리 super() 메소드 적용

--- src/main/java/numberCalculator/exceptions/InvalidFormatException.java
+++ src/main/java/numberCalculator/exceptions/InvalidFormatException.java
@@ -1,7 +1,12 @@
 package numberCalculator.exceptions;
 
 public class InvalidFormatException extends RuntimeException{
-    public InvalidFormatException() {
 
+    public InvalidFormatException() {
+        super("숫자/구분자가 잘못 작성되었습니다.");
+    }
+
+    public InvalidFormatException(String message) {
+        super(message);
     }
 }
