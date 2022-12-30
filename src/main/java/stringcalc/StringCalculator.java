1fd8cb2c89f6baa467e409f227f4a5b1c60e0249
HONGMINJU <mhong0800@gmail.com>
2022-12-30 10:22:07 +0900

    style: 함수를 static으로 변경, 함수명 변경

--- src/main/java/stringcalc/StringCalculator.java
+++ src/main/java/stringcalc/StringCalculator.java
@@ -4,21 +4,13 @@ import java.util.List;
 
 public class StringCalculator {
 
-    private final StringNumberParser stringNumberParser;
-    private final CustomSeparatorParser customSeparatorParser;
-
-    public StringCalculator() {
-        stringNumberParser = new StringNumberParser();
-        customSeparatorParser = new CustomSeparatorParser();
-    }
-
     private Long sum(List<Long> numbers) {
         return numbers.stream().reduce(0L, Long::sum);
     }
 
     public Long calculate(String input) {
-        String customSeparator = customSeparatorParser.parse(input);
-        List<Long> numbers = stringNumberParser.parse(input, customSeparator);
+        String customSeparator = CustomSeparatorParser.parseToCustomSeparator(input);
+        List<Long> numbers = StringNumberParser.parseToNumberList(input, customSeparator);
         return sum(numbers);
     }
 }
