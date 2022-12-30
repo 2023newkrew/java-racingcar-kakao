a6a9af9e0aaac68157b00037dfea5ecdfcdde150
ottoyoon98 <ottoyoon98@gmail.com>
2022-12-30 14:54:03 +0900

    refactor: 자동차 이름 등록 테스트 리팩토링

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -1,21 +1,30 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RacingTest {
-    @Test
-    public void registerCarNamesTest() {
-        Racing racing = new Racing();
-        assertTrue(racing.registerCarNames("pobi,crong,honux"));
-    }
 
-    @Test
-    public void registerCarNamesTest2() {
-        Racing racing = new Racing();
-        assertFalse(racing.registerCarNames("pobi,crong,honux12312"));
+    @Nested
+    @DisplayName("자동차 이름 등록 테스트")
+    class registerCarNamesTest {
+        @Test
+        @DisplayName("성공")
+        public void success() {
+            Racing racing = new Racing();
+            assertTrue(racing.registerCarNames("pobi,crong,honux"));
+        }
+
+        @Test
+        @DisplayName("5자 초과 이름 등록 실패")
+        public void fail() {
+            Racing racing = new Racing();
+            assertFalse(racing.registerCarNames("pobi,crong,honux12312"));
+        }
     }
 
     @Test
@@ -30,15 +39,6 @@ public class RacingTest {
         assertFalse(racing.registerCarRoundNum("abcd"));
     }
 
-    /*
-        @Test
-        public void RunTest(){
-            Racing racing = new Racing();
-            racing.RegisterCarRoundNum("5");
-            assertEquals(racing.Run(), 5);
-
-        }
-    */
     @Test
     public void RoundTest() {
         Racing racing = new Racing();
