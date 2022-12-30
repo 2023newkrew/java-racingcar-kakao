7e20293a288c20a8b7491dd9ed21d56303fd2430
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-30 15:23:00 +0900

    feat: RacingCar 이름에 대한 validation 로직 제거
    이름에 대한 validation 로직을 RacingCarName 객체에 위임했기 때문에
    view에서 제거함

--- src/main/java/racingcar/view/RacingGameInputView.java
+++ src/main/java/racingcar/view/RacingGameInputView.java
@@ -18,9 +18,7 @@ public class RacingGameInputView {
     public List<RacingCar> readRacingCars() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scanner.next();
-        String[] racingCarNames = input.split(",");
-        validateRacingCarNames(racingCarNames);
-        return convertToRacingCars(racingCarNames);
+        return convertToRacingCars(input.split(","));
     }
 
     public void close() {
@@ -31,17 +29,6 @@ public class RacingGameInputView {
         return Arrays.stream(racingCarNames)
                 .map(RacingCar::new)
                 .collect(Collectors.toList());
-    }
-
-    private void validateRacingCarNames(String[] racingCarNames) {
-        Arrays.stream(racingCarNames).
-                forEach(this::validateRacingCarName);
-    }
-
-    private void validateRacingCarName(String racingCarName) {
-        if (racingCarName.isBlank() || racingCarName.length() > 5) {
-            throw new IllegalArgumentException("잘못된 이름입니다!");
-        }
     }
 
     public int readRound() {
