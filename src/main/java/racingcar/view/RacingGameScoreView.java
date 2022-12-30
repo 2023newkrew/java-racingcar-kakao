c0728da627c1dad19b06ad0a6dfb70d4bf08ed54
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-30 14:58:33 +0900

    feat: RacingCar의 이름 변수를 RacingCarName 타입으로 변경

--- src/main/java/racingcar/view/RacingGameScoreView.java
+++ src/main/java/racingcar/view/RacingGameScoreView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCarName;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,9 +13,10 @@ public class RacingGameScoreView {
     }
 
     public void printWinners(List<RacingCar> racingCars) {
-        String winners = racingCars.stream().
-                map(RacingCar::getName).
-                collect(Collectors.joining(", "));
+        String winners = racingCars.stream()
+                .map(RacingCar::getRacingCarName)
+                .map(RacingCarName::getName)
+                .collect(Collectors.joining(", "));
         System.out.println(winners + "가 최종 우승했습니다.");
     }
 
