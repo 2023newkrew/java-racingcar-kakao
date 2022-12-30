4134eb0c8327b449f52ce49bde8ebf73f0ee99a8
queueSub <kyusch0330@gmail.com>
2022-12-30 14:45:09 +0900

    test: @BeforeEach를 사용하여 공통적으로 사용하는 객체 초기화

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.RacingCar;
@@ -8,45 +9,45 @@ import racingcar.domain.RacingGame;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class RacingGameTest {
+
+    RacingGame racingGame;
+
+    @BeforeEach
+    void setUp() {
+        //given
+        racingGame = new RacingGame(List.of("abc", "efg", "hijkl"), 5);
+    }
 
     @Test
     @DisplayName("한 턴을 플레이하는 기능")
     public void playTurn() {
-        //given
-        RacingGame racingGame = new RacingGame(List.of("abc","efg","hijkl"),5);
-
         //when
         racingGame.proceedTurn();
         racingGame.proceedTurn();
 
         //then
-        assertEquals(2,racingGame.getTurnCount());
+        assertEquals(2, racingGame.getTurnCount());
     }
 
     @Test
     @DisplayName("주어진 수 만큼 턴을 반복하는 기능")
     public void repeatPlayingTurn() {
-        //given
-        RacingGame racingGame = new RacingGame(List.of("abc","efg","hijkl"),5);
-
         //when
-        while(!racingGame.isFinished()) {
+        while (!racingGame.isFinished()) {
             racingGame.proceedTurn();
         }
 
         //then
-        assertEquals(5,racingGame.getTurnCount());
+        assertEquals(5, racingGame.getTurnCount());
     }
 
     @Test
     @DisplayName("게임 종료 후 우승자들을 판별해서 반환하는 기능")
     public void judgeWinners() {
-        //given
-        RacingGame racingGame = new RacingGame(List.of("abc","efg","hijkl"),5);
-
         //when
-        while(!racingGame.isFinished()) {
+        while (!racingGame.isFinished()) {
             racingGame.proceedTurn();
         }
 
@@ -55,13 +56,13 @@ public class RacingGameTest {
         // then
         // 위너끼리 pos가 같은지 확인
         int winnerPos = winners.get(0).getPos();
-        for(RacingCar rc : winners) {
+        for (RacingCar rc : winners) {
             assertEquals(winnerPos, rc.getPos());
         }
 
         // 위너가 아닌 차들의 pos가 위너의 pos보다 작은지 확인
-        for(RacingCar rc : racingGame.getCars()) {
-            if(winners.contains(rc)) continue;
+        for (RacingCar rc : racingGame.getCars()) {
+            if (winners.contains(rc)) continue;
             assertTrue(rc.getPos() < winnerPos);
         }
     }
