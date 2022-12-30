4a4343cc9a2a25f6e9e9fe3758ed3308e98d07f3
queueSub <kyusch0330@gmail.com>
2022-12-30 15:03:37 +0900

    test: 자동차 이름 분리 기능 테스트 RacingCarNameTest로 이동

--- src/test/java/racingcar/RacingCarNameTest.java
+++ src/test/java/racingcar/RacingCarNameTest.java
@@ -1,9 +1,13 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.car.RacingCarName;
+import racingcar.view.InputView;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +25,17 @@ public class RacingCarNameTest {
     @ValueSource(strings = {"jack,,,gene", "jack,oscar.award,tom", "tom,matt,wronglongname"})
     public void validateNameException(final String name) {
         assertThrows(IllegalArgumentException.class, () -> RacingCarName.parseCarNames(name));
+    }
+
+    @Test
+    @DisplayName("자동차 이름들을 쉼표 기준으로 분리하여 반환하는 기능 테스트")
+    public void inputCarsName() {
+        //when
+        List<String> carNames = RacingCarName.parseCarNames("oscar,gene,aiden");
+
+        //then
+        assertEquals("oscar",carNames.get(0));
+        assertEquals("gene",carNames.get(1));
+        assertEquals("aiden",carNames.get(2));
     }
 }
