6cd237d63cf3949cdabfaefc27532a79561da1c1
queueSub <kyusch0330@gmail.com>
2022-12-30 14:39:46 +0900

    test: 자동차 이름 유효성 검사 테스트 추가

--- src/main/java/racingcar/domain/RacingCarName.java
+++ src/main/java/racingcar/domain/RacingCarName.java
@@ -16,14 +16,14 @@ public class RacingCarName {
 
     public static List<String> parseCarNames(String carNamesInput) {
         List<String> carNames = Arrays.asList(carNamesInput.split(","));
-        for(String carName : carNames) {
+        for (String carName : carNames) {
             validateCarName(carName);
         }
         return carNames;
     }
 
     private static void validateCarName(String carName) {
-        if(carName.isEmpty() || carName.length()>5)
-            throw new IllegalArgumentException("차 이름은 5글자 이하로 작성해주세요.");
+        if (carName.isEmpty() || carName.length() > 5)
+            throw new IllegalArgumentException("차 이름은 1~5 글자로 입력해주세요.");
     }
 }
---
+++ src/test/java/racingcar/RacingCarNameTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.RacingCarName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RacingCarNameTest {
+
+    @ParameterizedTest(name = "name : {0}")
+    @DisplayName("이름 유효성 검사 통과 테스트")
+    @ValueSource(strings = {"aiden", "jack,oscar,jerry", "tom,matt,right"})
+    public void validateNamePass(final String name) {
+        assertDoesNotThrow(() -> RacingCarName.parseCarNames(name));
+    }
+
+    @ParameterizedTest(name = "name : {0}")
+    @DisplayName("이름 글자 수 예외 테스트")
+    @ValueSource(strings = {"jack,,,gene", "jack,oscar.award,tom", "tom,matt,wronglongname"})
+    public void validateNameException(final String name) {
+        assertThrows(IllegalArgumentException.class, () -> RacingCarName.parseCarNames(name));
+    }
+}
