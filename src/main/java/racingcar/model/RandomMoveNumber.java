99e94ba86ba0869f6da1e4f1f4926b98155c1f37
gkwodn1044 <gkwodn1044@gmail.com>
2022-12-30 13:47:33 +0900

    feat : RandomMoveNumber 클래스 random 클래스 변수 final 추가

--- src/main/java/racingcar/model/RandomMoveNumber.java
+++ src/main/java/racingcar/model/RandomMoveNumber.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomMoveNumber {
     public static int RANDOM_MAX_LIMIT = 10;
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     public static int getRandomMoveNumber() {
         return random.nextInt(RANDOM_MAX_LIMIT);
