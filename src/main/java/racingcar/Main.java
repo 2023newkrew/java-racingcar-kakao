5657b49c5e007cd746d5d20fcbf35fdf655b1685
Sangyups <feels4637@gmail.com>
2022-12-30 23:00:10 +0900

    feat: Controller 로 View 와 Domain 연결

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -1,8 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+
 public class Main {
 
     public static void main(String[] args) {
+        RacingController racingController = new RacingController();
 
+        racingController.startRace();
     }
 }
