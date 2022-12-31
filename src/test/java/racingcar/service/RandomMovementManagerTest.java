9083bc8214bce4313a535075fc3ff5733595b3fc
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-31 17:13:57 +0900

    feat: MOVEMENT_THRESHOLD 상수 값을 public으로 변경

--- src/test/java/racingcar/service/RandomMovementManagerTest.java
+++ src/test/java/racingcar/service/RandomMovementManagerTest.java
@@ -1,13 +1,15 @@
 package racingcar.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomMovementManagerTest {
 
-    @DisplayName("랜덤값이 4 이상일 경우 true를 반환한다")
+    @DisplayName("랜덤값이 " + RandomMovementManager.MOVEMENT_THRESHOLD + " 이상일 경우 true를 반환한다")
     @Test
     void makeMovementDecision_true(){
         // given
@@ -20,7 +22,7 @@ class RandomMovementManagerTest {
         assertThat(isMove).isTrue();
     }
 
-    @DisplayName("랜덤값이 4 미만일 경우 false를 반환한다")
+    @DisplayName("랜덤값이 "+ RandomMovementManager.MOVEMENT_THRESHOLD + " 미만일 경우 false를 반환한다")
     @Test
     void makeMovementDecision_false(){
         // given
