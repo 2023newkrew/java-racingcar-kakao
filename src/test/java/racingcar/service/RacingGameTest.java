7b0b9adb5b57bbfce91c3d5e90185ddf760e61a6
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-31 16:45:33 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/racingcar/service/RacingGameTest.java
+++ src/test/java/racingcar/service/RacingGameTest.java
@@ -3,12 +3,7 @@ package racingcar.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
