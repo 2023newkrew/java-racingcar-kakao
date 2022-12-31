63c0717c4f0a548edc357a8f51235d22044851d2
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-31 16:28:35 +0900

    test: RacingCars의 toString() 테스트

--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -18,5 +19,21 @@ class RacingCarsTest {
     @Test
     void create_throwEx() {
         assertThatThrownBy(() -> new RacingCars(new String[]{"longName"})).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차들의 이름과 distance를 -로 출력한다")
+    @Test
+    void printScore() {
+        //given
+        String[] racingCarNames = new String[]{"car1", "car2", "car3"};
+        String expected = "car1 : -\n" +
+                "car2 : -\n" +
+                "car3 : -";
+
+        //when
+        RacingCars racingCars = new RacingCars(racingCarNames);
+
+        //then
+        assertThat(racingCars.toString()).hasToString(expected);
     }
 }
