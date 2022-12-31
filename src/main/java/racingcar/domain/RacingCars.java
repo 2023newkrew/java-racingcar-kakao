75b453eab89f21e7b5cab4890e666545397f63af
Kloong1 <dkdlsmsm98@gmail.com>
2022-12-31 15:07:59 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import racingcar.service.RandomMovementManager;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
