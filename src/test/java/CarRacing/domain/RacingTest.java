c24d8467b15023c554472ed1432fe8aac1225fbf
Seongsu Park <86999506+HolywaterPark@users.noreply.github.com>
2023-01-01 21:05:26 +0900

    refac: 누락되어 있던 RacingCount 검증 로직 테스트 코드 작성

--- src/test/java/CarRacing/domain/RacingTest.java
+++ src/test/java/CarRacing/domain/RacingTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RacingTest {
     private Racing racing;
@@ -20,6 +22,21 @@ public class RacingTest {
         racingWithCars = new Racing(cars, 5);
     }
 
+    @DisplayName("레이싱 시도 횟수가 정상인 경우 테스트")
+    @Test
+    public void correctRacingCountTest() {
+        assertDoesNotThrow(() -> {
+            racing.validateRacingCount("5");
+        });
+    }
+
+    @DisplayName("레이싱 시도 횟수가 비정상인 경우 테스트")
+    @Test
+    public void wrongRacingCountTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            racing.validateRacingCount("abc");
+        });
+    }
     @DisplayName("이름 배열로 자동차 배열을 생성한 후 길이가 일치하는지 테스트")
     @Test
     public void makeCarListTest() {
