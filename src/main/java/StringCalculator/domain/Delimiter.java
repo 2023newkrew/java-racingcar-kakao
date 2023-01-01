a53b56ba30c57de012db793f1d4adb2316c0c64f
jerrie.choi <pinkytime96@gmail.com>
2023-01-01 15:57:49 +0900

    refac: 커스텀 구분자 추출을 위한 pattern 상수화

--- src/main/java/StringCalculator/domain/Delimiter.java
+++ src/main/java/StringCalculator/domain/Delimiter.java
@@ -6,15 +6,14 @@ import java.util.regex.Pattern;
 public class Delimiter {
 
     private final static String CUSTOM_DELIMITER_REGEX = "//(.)\\\\n(.*)";
+    private final static Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);;
     private final static int CUSTOM_DELIMITER_INDEX = 1;
     private final static int CUSTOM_INPUTSTRING_INDEX = 2;
 
     private String delimiterRegEx = ",|:";
     private String inputString;
-    private Pattern pattern;
 
     public Delimiter(String inputString) {
-        pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
         Matcher matcher = pattern.matcher(inputString);
         this.inputString = inputString;
         if (matcher.find()) {
