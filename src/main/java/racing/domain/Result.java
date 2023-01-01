067e16ff304500e618071f897918207db006a68e
jerrie.choi <pinkytime96@gmail.com>
2023-01-01 10:50:17 +0900

    refac: Result 클래스 캡슐화

--- src/main/java/racing/domain/Result.java
+++ src/main/java/racing/domain/Result.java
@@ -7,20 +7,22 @@ import java.util.stream.Collectors;
 
 public class Result {
 
-    private List<String> getCarsWithPosition(Car[] cars, int position) {
-        List<String> winners = Arrays.stream(cars)
+    private List<String> winners = new ArrayList<>();
+
+    private void getCarsWithPosition(Car[] cars, int position) {
+        winners = Arrays.stream(cars)
                 .filter(car -> car.getPosition() == position)
                 .map(s -> s.getName()).collect(Collectors.toList());
-        return winners;
     }
 
     public List<String> getWinner(CarList carList) {
-        List<String> winners = new ArrayList<>();
+        winners = new ArrayList<>();
         int maxPosition = Arrays.stream(carList.getCars())
                 .mapToInt(Car::getPosition)
                 .max()
                 .orElse(0);
-        return getCarsWithPosition(carList.getCars(), maxPosition);
+        getCarsWithPosition(carList.getCars(), maxPosition);
+        return winners;
     }
 
 }
