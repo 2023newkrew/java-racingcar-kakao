b4607a0ed80e86615521cdbb6d478b44c8f583f4
pyun0825 <pyun0825@naver.com>
2023-01-01 23:21:51 +0900

    test(racing): add CarCreatorTest

---
+++ src/test/java/carracing/CarCreatorTest.java
@@ -0,0 +1,34 @@
+package carracing;
+
+import carracing.domain.CarCreator;
+import carracing.domain.RacingPlayer;
+import carracing.domain.StringParser;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+class CarCreatorTest {
+    CarCreator carCreator;
+
+    @BeforeEach
+    void init() {
+        StringParser stringParser = new StringParser();
+        carCreator = new CarCreator(stringParser);
+    }
+
+    @DisplayName("Car 객체 생성 후 이름 올바른지 검사")
+    @Test
+    void createCars_validInput() {
+        //given
+        String playerNames = "a,b,c";
+        //when
+        List<RacingPlayer> cars = carCreator.createCars(playerNames);
+        List<String> carNames = cars.stream().map(RacingPlayer::getName).collect(Collectors.toList());
+        //then
+        Assertions.assertThat(carNames).containsExactly("a", "b", "c");
+    }
+}
