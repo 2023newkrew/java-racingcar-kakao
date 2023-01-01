45099450fca3c3c402c61a9604a45cb5782cfcd0
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-01 22:26:31 +0900

    refactor: 사용하지 않는 import 삭제

--- src/test/java/racingcar/service/RandomMovementManagerTest.java
+++ src/test/java/racingcar/service/RandomMovementManagerTest.java
@@ -1,8 +1,6 @@
 package racingcar.service;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
