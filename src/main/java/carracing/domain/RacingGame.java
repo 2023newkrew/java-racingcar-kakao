c98ab14482a00f4fc4e59783a96b31702743655f
pyun0825 <pyun0825@naver.com>
2023-01-01 23:35:10 +0900

    test(racing): add RacingGameTest

--- src/main/java/carracing/domain/RacingGame.java
+++ src/main/java/carracing/domain/RacingGame.java
@@ -1,6 +1,5 @@
 package carracing.domain;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
---
+++ src/test/java/carracing/RacingGameTest.java
@@ -0,0 +1,42 @@
+package carracing;
+
+import carracing.domain.Car;
+import carracing.domain.RacingGame;
+import carracing.domain.RacingPlayer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+class RacingGameTest {
+
+    @DisplayName("턴 진행 후 턴 카운트가 올라갔는지 & 게임 끝 판정 검사")
+    @Test
+    void runSingleTurnAndIsFinished() {
+        //given
+        List<RacingPlayer> racingPlayers = new ArrayList<>();
+        racingPlayers.add(new Car("a"));
+        racingPlayers.add(new Car("b"));
+        RacingGame racingGame = new RacingGame(racingPlayers, 1);
+        //when
+        racingGame.runSingleTurn();
+        //then
+        Assertions.assertThat(racingGame.isFinished()).isTrue();
+    }
+
+    @DisplayName("포지션 높은 선수가 우승자로 판정되는지 검사")
+    @Test
+    void getWinners() {
+        //given
+        List<RacingPlayer> racingPlayers = new ArrayList<>();
+        Car carA = new Car("a", 3);
+        Car carB = new Car("a", 1);
+        racingPlayers.add(carA);
+        racingPlayers.add(carB);
+        RacingGame racingGame = new RacingGame(racingPlayers, 1);
+        //when
+        //then
+        Assertions.assertThat(racingGame.getWinners()).containsExactly(carA);
+    }
+}
