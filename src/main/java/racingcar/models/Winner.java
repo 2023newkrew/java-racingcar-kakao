ada295c3eebe18282686b3a5536d17ebc5d92fa5
park0503 <hooo0503@gmail.com>
2023-01-02 11:07:42 +0900

    refactor: 우승자 탐색 로직 Winner 클래스로 분리

--- src/main/java/racingcar/models/Winner.java
+++ src/main/java/racingcar/models/Winner.java
@@ -1,15 +1,26 @@
 package racingcar.models;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Winner {
 
+    public final List<Car> winners;
 
     public Winner(List<Car> cars) {
+        this.winners = findWinners(cars);
+    }
+
+    private List<Car> findWinners(List<Car> cars) {
+        return cars.stream().filter((car) -> car.throughSpecificPoint(getMaxPosition(cars)))
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxPosition(List<Car> cars) {
+        return cars.stream().mapToInt(Car::getPosition).max().orElse(0);
     }
 
     public List<String> getWinnerNames() {
-        return new ArrayList<>();
+        return winners.stream().map(Car::getName).collect(Collectors.toList());
     }
 }
