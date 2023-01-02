57cdf7e038f6f340070550d57d6e142bd942f8bf
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-03 01:28:40 +0900

    refactor: DelimiterParser 고정 값 상수처리, 상수명 변경
    - REGEX -> CUSTOM_DELIMITER_REGEX
    - group(1) -> group(CUSTOM_DELIMITER_INDEX)
    - group(2) -> group(PURIFIED_INPUT_INDEX)

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -6,18 +6,20 @@ import java.util.regex.Pattern;
 
 public class DelimiterParser {
 
-    private static final String REGEX = "//(.)\n(.*)"; // (.): 사용자 정의 구분자 (.*): 구분자로 나눌 문자열
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)"; // (.): 사용자 정의 구분자 (.*): 구분자로 나눌 문자열
+    public static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int PURIFIED_INPUT_INDEX = 2;
     private final String delimiter;
     private final boolean hasCustomDelimiter;
     private final String processedInput;
 
     public DelimiterParser(String pureInput, List<String> defaultDelimiters) {
 
-        Matcher matcher = Pattern.compile(REGEX).matcher(pureInput);
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(pureInput);
         if (matcher.find()) {
             this.hasCustomDelimiter = true;
-            this.delimiter = matcher.group(1);
-            this.processedInput = matcher.group(2);
+            this.delimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
+            this.processedInput = matcher.group(PURIFIED_INPUT_INDEX);
             return;
         }
         this.hasCustomDelimiter = false;
