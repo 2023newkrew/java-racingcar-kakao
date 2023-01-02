8c48027add8e73ef181fb3a808bcab3df1a45da8
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-02 15:56:54 +0900

    feat : NameStringVerify 클래스 verifyBlankNameString 메소드 분리 NameStringVerify 클래스 verifyEachCarName 메소드 분리 NameStringVerify 클래스 DEFAULT_DELIMITER 상수 오타 수정

--- src/main/java/racingcar/model/NameStringVerify.java
+++ src/main/java/racingcar/model/NameStringVerify.java
@@ -1,14 +1,21 @@
 package racingcar.model;
 
 public class NameStringVerify {
-    private static final String DEFAULT_DELIMETER = ",";
+    private static final String DEFAULT_DELIMITER = ",";
 
     public static void verifyNameString(String nameStr) {
+        verifyBlankNameString(nameStr);
+        verifyEachCarName(nameStr);
+    }
+
+    private static void verifyBlankNameString(String nameStr) {
         if (nameStr == null || nameStr.isBlank()) {
             throw new IllegalArgumentException("There should be at least 1 car name in string");
         }
+    }
 
-        final String[] nameArray = nameStr.split(DEFAULT_DELIMETER, -1);
+    private static void verifyEachCarName(String nameStr) {
+        final String[] nameArray = nameStr.split(DEFAULT_DELIMITER, -1);
 
         for (String name : nameArray) {
             CarNameVerify.verifyCarName(name);
