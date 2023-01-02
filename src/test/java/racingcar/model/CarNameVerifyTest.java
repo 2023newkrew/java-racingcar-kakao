372e503358857886927f3301fb281270bbdb3aec
gkwodn1044 <gkwodn1044@gmail.com>
2023-01-02 15:42:07 +0900

    feat : CarNameVerifyTest longCarNameTest 테스트 생성

--- src/test/java/racingcar/model/CarNameVerifyTest.java
+++ src/test/java/racingcar/model/CarNameVerifyTest.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -11,6 +12,14 @@ public class CarNameVerifyTest {
     @NullAndEmptySource
     @ParameterizedTest
     void blackCarNameTest(final String name) {
+        assertThatThrownBy(() -> CarNameVerify.verifyCarName(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Throw exception when car name is longer than 5")
+    @ValueSource(strings = {"testtt", "testttt", "testtttt", "testttttt"})
+    @ParameterizedTest
+    void longCarNameTest(final String name) {
         assertThatThrownBy(() -> CarNameVerify.verifyCarName(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
