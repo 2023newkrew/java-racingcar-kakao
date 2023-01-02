5602ab8900c15fdc2bac8aa70b6e1c3f72d84aad
ottoyoon98 <ottoyoon98@gmail.com>
2023-01-03 04:58:29 +0900

    Refactor: MVC에 맞게 리팩토링 - Main이 컨트롤러의 역할을 하도록 함. - Calculator가 뷰의 역할을 하도록 다른 기능 제거.

--- src/main/java/addcal/Calculator.java
+++ src/main/java/addcal/Calculator.java
@@ -3,37 +3,16 @@ package addcal;
 import java.util.Scanner;
 
 public class Calculator {
-    public AddCal addCal;
 
     public String getCalculatorInput() {
         System.out.println("입력하세요: ");
         Scanner sc = new Scanner(System.in);
-        String input = "";
-
-        do {
-            input = sc.nextLine();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while (inputValid(input));
-        return input;
+        return sc.nextLine();
     }
 
-    public void printCalculatorOutput() {
+    public void printCalculatorOutput(AddCal addCal) {
         addCal.printResult();
         System.out.println("End");
-    }
-
-    public boolean run(String input) {
-        addCal = new AddCal(input);
-        printCalculatorOutput();
-        return true;
-    }
-
-    private Boolean inputValid(String input) {
-        return false;
     }
 
 }
--- src/main/java/addcal/Main.java
+++ src/main/java/addcal/Main.java
@@ -3,6 +3,15 @@ package addcal;
 public class Main {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-        cal.run(cal.getCalculatorInput());
+        AddCal addCal;
+        while (true) {
+            try {
+                addCal = new AddCal(cal.getCalculatorInput());
+                cal.printCalculatorOutput(addCal);
+                break;
+            } catch (Exception E) {
+                System.out.println(E);
+            }
+        }
     }
 }
