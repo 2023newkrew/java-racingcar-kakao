bfcb0946e7bfb7445939b6f84264feacd1a13aab
park0503 <hooo0503@gmail.com>
2023-01-02 11:55:21 +0900

    test: 테스트명과 DisplayName 분리

--- src/test/java/racingcar/WinnerTest.java
+++ src/test/java/racingcar/WinnerTest.java
@@ -2,12 +2,14 @@ package racingcar;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.models.Car;
 import racingcar.models.Winner;
 
 public class WinnerTest {
     @Test
+    @DisplayName("우승자가 한명일 때 테스트")
     public void testGetSingleWinner() {
         List<Car> cars = List.of(new Car("jaws", 12), new Car("jayde", 10), new Car("musc", 11));
         Winner winner = new Winner(cars);
@@ -15,6 +17,7 @@ public class WinnerTest {
     }
 
     @Test
+    @DisplayName("우승자가 여러명일 때 테스트")
     public void testMultipleWinners() {
         List<Car> cars = List.of(new Car("jaws", 12), new Car("jayde", 12), new Car("musc", 12));
         Winner winner = new Winner(cars);
