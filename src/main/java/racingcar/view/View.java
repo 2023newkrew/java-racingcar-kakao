b57003e4eed93e90e017bd9bbe93f2d91313b8ba
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-03 17:26:57 +0900

    refactor: 안내메시지 출력 부분 이동

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -5,6 +5,14 @@ import racingcar.model.Car;
 import java.util.List;
 
 public class View {
+    public void printInputNameMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public void printInputRoundNumberMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
     public void printRoundResult(List<Car> roundResult) { //라운드의 결과를 출력한다
         for (Car car : roundResult) {
             System.out.print(car.getName() + " : ");
