bec6f6a3452d710b93316477e17e6fb99484b483
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-03 16:47:41 +0900

    refactor: main의 기능을 생성자로 이동

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,6 +1,6 @@
 package racingcar.controller;
 
-import racingcar.domain.Racing;
+import racingcar.model.Racing;
 import racingcar.dto.InputDto;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -8,24 +8,7 @@ import racingcar.view.OutputView;
 import java.util.List;
 
 public class RacingController {
-    private static Racing settings() { // 게임에 필요한 정보를 받아오고, 레이싱 객체를 생성한다
-        InputDto inputDto = InputView.creatInput();
-
-        List<String> carNames = inputDto.getCarNames();
-        int roundNum = inputDto.getRoundNum();
-
-        return new Racing(carNames, roundNum);
-    }
-
-    private static void printRoundResult(Racing racing) { //라운드 진행 후, 결과를 출력한다
-        OutputView.printRoundResult(racing.returnCarState().getCarState());
-    }
-
-    private static void printWinnerResult(Racing racing) { //우승자를 출력한다
-        OutputView.printWinner(racing.returnWinners().getWinners());
-    }
-
-    public static void main(String[] args) {
+    public RacingController() {
         Racing racing = settings();
 
         while (!racing.isEnd()) {
@@ -34,4 +17,21 @@ public class RacingController {
         }
         printWinnerResult(racing);
     }
+    private Racing settings() { // 게임에 필요한 정보를 받아오고, 레이싱 객체를 생성한다
+        InputDto inputDto = InputView.creatInput();
+
+        List<String> carNames = inputDto.getCarNames();
+        int roundNum = inputDto.getRoundNum();
+
+        return new Racing(carNames, roundNum);
+    }
+
+    private void printRoundResult(Racing racing) { //라운드 진행 후, 결과를 출력한다
+        OutputView.printRoundResult(racing.returnCarState().getCarState());
+    }
+
+    private void printWinnerResult(Racing racing) { //우승자를 출력한다
+        OutputView.printWinner(racing.returnWinners().getWinners());
+    }
+
 }
