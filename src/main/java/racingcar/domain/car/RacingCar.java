d67a991ff0afe92f2e0d283d208a6689d85e324b
queueSub <kyusch0330@gmail.com>
2023-01-03 17:52:14 +0900

    refactor: 기본 차 이름 매직 리터럴을 상수로 선언

--- src/main/java/racingcar/domain/car/RacingCar.java
+++ src/main/java/racingcar/domain/car/RacingCar.java
@@ -3,13 +3,15 @@ package racingcar.domain.car;
 import racingcar.domain.power.PowerGeneratable;
 
 public class RacingCar {
+
+    private static final String DEFAULT_CAR_NAME = "car";
     private final static int POWER_THRESHOLD = 4;
     private final static int MOVE_DISTANCE = 1;
     private final RacingCarName carName;
     private int pos;
 
     public RacingCar() {
-        this("car");
+        this(DEFAULT_CAR_NAME);
     }
 
     public RacingCar(String carName) {
