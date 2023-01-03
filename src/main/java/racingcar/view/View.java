2e0b1b5c2a041a47b789c6d2bcae0ed554d98471
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-03 17:24:20 +0900

    refactor: 클래스 이름 수정, 스태틱 메서드 인스턴스 메서드로 수정

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -4,8 +4,8 @@ import racingcar.model.Car;
 
 import java.util.List;
 
-public class OutputView {
-    public static void printRoundResult(List<Car> roundResult) { //라운드의 결과를 출력한다
+public class View {
+    public void printRoundResult(List<Car> roundResult) { //라운드의 결과를 출력한다
         for (Car car : roundResult) {
             System.out.print(car.getName() + " : ");
             System.out.println("-".repeat(car.getPosition()));
@@ -13,7 +13,7 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printWinner(List<String> winners) { //경기의 승자를 출력한다
+    public void printWinner(List<String> winners) { //경기의 승자를 출력한다
         System.out.print(String.join(", ", winners));
         System.out.println("가 최종 우승했습니다.");
     }
