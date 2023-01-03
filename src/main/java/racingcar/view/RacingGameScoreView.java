bc5eb186de65c42fc27fecb5e94321a29e394db9
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-03 14:26:24 +0900

    feat: 우승자를 출력할 때 toString 대신 getter를 사용하도록 변경

--- src/main/java/racingcar/view/RacingGameScoreView.java
+++ src/main/java/racingcar/view/RacingGameScoreView.java
@@ -1,7 +1,12 @@
 package racingcar.view;
 
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCarName;
 import racingcar.domain.RacingCars;
 import racingcar.domain.RacingGameWinners;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGameScoreView {
     public void printRacingCarDist(RacingCars racingCars) {
@@ -9,7 +14,12 @@ public class RacingGameScoreView {
     }
 
     public void printWinners(RacingGameWinners winners) {
-        System.out.println(winners);
+        List<RacingCar> winnerCars = winners.getWinners();
+        String winnerNames = winnerCars.stream()
+                .map(RacingCar::getRacingCarName)
+                .map(RacingCarName::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
     }
 
     public void printStartMessage(RacingCars racingCars) {
