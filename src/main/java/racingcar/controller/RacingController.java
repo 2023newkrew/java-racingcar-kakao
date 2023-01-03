767ff859f6775f1a35ba293ca404f49b13ca8196
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-01-03 17:31:04 +0900

    refactor: 사용자 입력을 받는 부분을 컨트롤러로 이동, racing 객체를 컨트롤러가 가지도록 수정

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,37 +1,72 @@
+/*
+사용자의 입력을 가장 처음 받는 것이 여기임
+뷰는.. 모델이 변화되었을 때 보여주는 것만 기능하고..
+
+모델: 레이싱 객체를 가진다.
+뷰: 레이싱 상태가 변경될 때 마다 화면에 출력하는 역할을 한다.
+컨트롤러: 입력에 대한 응답으로 모델 이나 뷰를 업데이트 한다.
+
+ */
 package racingcar.controller;
 
 import racingcar.model.Racing;
 import racingcar.dto.InputDto;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+import racingcar.utils.Utils;
+import racingcar.utils.Validate;
+import racingcar.view.View;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class RacingController {
+    private final Racing racing;
+    private final View view;
     public RacingController() {
-        Racing racing = settings();
+        racing = settings();
+        view = new View();
 
         while (!racing.isEnd()) {
             racing.round();
-            printRoundResult(racing);
+            printRoundResult();
         }
-        printWinnerResult(racing);
+        printWinnerResult();
     }
-    private Racing settings() { // 게임에 필요한 정보를 받아오고, 레이싱 객체를 생성한다
-        InputDto inputDto = InputView.creatInput();
-
-        List<String> carNames = inputDto.getCarNames();
-        int roundNum = inputDto.getRoundNum();
-
-        return new Racing(carNames, roundNum);
+    private Racing settings() { //유저 입력을 받으며 게임 시작
+        InputDto inputDto = creatInput();
+        return new Racing(inputDto.getCarNames(), inputDto.getRoundNum());
     }
 
-    private void printRoundResult(Racing racing) { //라운드 진행 후, 결과를 출력한다
-        OutputView.printRoundResult(racing.returnCarState().getCarState());
+    private void printRoundResult() { //라운드 진행 후, 결과를 출력한다
+        view.printRoundResult(racing.returnCarState().getCarState());
     }
 
-    private void printWinnerResult(Racing racing) { //우승자를 출력한다
-        OutputView.printWinner(racing.returnWinners().getWinners());
+    private void printWinnerResult() { //우승자를 출력한다
+        view.printWinner(racing.returnWinners().getWinners());
+    }
+    private List<String> inputCarNames() { //사용자의 입력을 받는다(자동차 이름)
+        Scanner sc = new Scanner(System.in);
+        String nameInput;
+        do {
+            view.printInputNameMessage();
+            nameInput = sc.nextLine();
+        } while (!Validate.validateCarNames(nameInput));
+        return Utils.stringToStringList(nameInput);
+    }
+
+    private int inputRoundNum() { //사용자의 입력을 받는다(라운드 횟수)
+        Scanner sc = new Scanner(System.in);
+        String roundNumInput;
+        do {
+            view.printInputRoundNumberMessage();
+            roundNumInput = sc.nextLine();
+        } while (!Validate.validateRoundNumber(roundNumInput));
+        return Utils.stringToInt(roundNumInput);
+    }
+
+    public InputDto creatInput() { //입력받는 메서드를 호출하고, dto를 리턴한다
+        List<String> carNames = inputCarNames();
+        int roundNum = inputRoundNum();
+        return new InputDto(carNames, roundNum);
     }
 
 }
