88c75feeebaeba5c0ed8e069fb45df7e472cf00e
jade.xyz <jade.xyz@kakaocorp.com>
2023-01-04 15:41:06 +0900

    refactor(Util): 객체 생성 시 예외처리 추가
    시도 횟수인 turn이 음수인 경우 객체가 생성되지 않도록 예외처리 추가

--- src/main/java/racingcar/step2/domain/RacingGame.java
+++ src/main/java/racingcar/step2/domain/RacingGame.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 public class RacingGame {
 
+    private static final String ERR_TURN_MUST_BE_POSITIVE = "시도 횟수는 0보다 커야 합니다.";
+
     private final Cars cars;
     private int turn;
 
     public RacingGame(List<String> carNames, int turn) {
         this.cars = new Cars(carNames);
+
+        if (turn < 0) {
+            throw new IllegalArgumentException(ERR_TURN_MUST_BE_POSITIVE);
+        }
         this.turn = turn;
     }
 
